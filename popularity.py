93e9cb147c9d2050ef6d99eec7d865bc3c1de63f
Mayank Jaiswal <msjaiswal@gmail.com>
2017-05-11 12:08:18 +0530

    popularity

--- popularity.py
+++ popularity.py
@@ -42,29 +42,15 @@ def valid_date(s):
     raise argparse.ArgumentTypeError(msg)
 
 
-#def valid_month(s):
-#  try:
-#    if re.search("^-?[0-9]+$", s):
-#      return now.replace().format('YYYY-MM-DD')
-#    else:
-#      raise Exception("Bad month format")
-#      return arrow.get(s, 'YYYY-MM-DD').format('YYYY-MM-DD')
-#      #return datetime.datetime.strptime(s, "%Y-%m-%d")
-#  except ValueError:
-#    msg = "Not a valid date: '{0}'.".format(s)
-#    raise argparse.ArgumentTypeError(msg)
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--num-prods", '-n', required=True, help="Number of products to be fetched from omniture. Pass 0 for fetching all.", default=0, type=int)
 parser.add_argument("--yes", '-y', help="Pass this to avoid prompt and run full report", action='store_true')
 parser.add_argument("--startdate", help="startdate in YYYYMMDD format or number of days to add from today i.e -4", type=valid_date, default=arrow.now().replace(days=-30).format('YYYY-MM-DD'))
 parser.add_argument("--enddate", help="enddate in YYYYMMDD format or number of days to add from today i.e -4", type=valid_date, default=arrow.now().replace().format('YYYY-MM-DD'))
-#parser.add_argument("--month", help="month in YYYYMM format", type=valid_date, default=arrow.now().replace().format('YYYY-MM-DD'))
-
-#parser.add_argument("--run-report", action='store_true', help="Used for development. Uses sample report data.")
 parser.add_argument("--fetch-omniture", help="Only runs the report and prints.", action='store_true')
 parser.add_argument("--preprocess", help="Only runs the report and prints.", action='store_true')
 parser.add_argument("--popularity", help="Calculates popularity", action='store_true')
+parser.add_argument("--post-to-solr", help="Posts popularity to Solr", action='store_true')
 parser.add_argument("--dump-metrics", help="Dump metrics into a file", action='store_true')
 
 parser.add_argument("--from-file", help="Read report from file", type=str)
@@ -90,7 +76,9 @@ if argv['num_prods'] and argv['fetch_omniture']== 0:
 
 if argv['fetch_omniture'] or argv['dump_metrics']:
   analytics = omniture.authenticate('soumen.seth:FSN E-Commerce', '770f388b78d019017d5e8bd7a63883fb')
-  suite = analytics.suites['fsnecommerceprod']
+  suites = {}
+  suites['web'] = analytics.suites['fsnecommerceprod']
+  suites['mobile'] = analytics.suites['fsnecommercemobileappprod']
 
 if argv['dump_metrics']:
   dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -130,55 +118,48 @@ def fetch_data():
   else:
     top = min(total_rows, 50000)
 
-  #print("total_rows: %s top=%s" %(total_rows, top))
-  startingWith = 0 
-  report_cnt = 1
-  while(True):
-    print("== report  %d ==" % report_cnt)
-    report_cnt += 1
-    report = suite.report \
-        .metric('event5') \
-        .metric('cartadditions') \
-        .metric('orders') \
-        .element('product', top=top, startingWith=startingWith)\
-        .element('category')\
-        .range(argv['startdate'], argv['enddate'])\
-        .granularity("day")\
-        .run()
-    print(" --- ")
-    #print(report)
-    data = report.data
-    if argv['fetch_omniture']:
-      print(data)
-    for product in data:
-      if 'datetime' in product:
-        product['date'] = datetime.datetime.strftime(product['datetime'], '%Y-%m-%d')
-        #product.pop("datetime")
-      yield product
+  for platform in ['web', 'mobile']:
+    print("== %s ==" % platform)
+    startingWith = 0 
+    report_cnt = 1
+    while(True):
+      print("== report  %d ==" % report_cnt)
+      report_cnt += 1
+      report = suites[platform].report \
+          .metric('event5') \
+          .metric('cartadditions') \
+          .metric('orders') \
+          .element('product', top=top, startingWith=startingWith)\
+          .element('category')\
+          .range(argv['startdate'], argv['enddate'])\
+          .granularity("day")\
+          .run()
+      print(" --- ")
+      data = report.data
+      for product in data:
+        if 'datetime' in product:
+          product['date'] = datetime.datetime.strftime(product['datetime'], '%Y-%m-%d')
+          product[platform] = platform
+        yield product
 
-    if len(data) < top or (total_rows and top * report_cnt > total_rows):
-      break
-    startingWith += top
+      if len(data) < top or (total_rows and top * report_cnt > total_rows):
+        break
+      startingWith += top
 
 
 def write_report_data_to_db():
 
   for product in fetch_data():
-    #print(product['product'])
     d = {
       "views": product['event5'],
       "cart_additions": product['cartadditions'],
       "orders": product['orders'],
       "productid": product['product'],
-      "date" : product['datetime']
+      "date" : product['datetime'],
+      "platform": product['platform']
     }
 
-    #d['cart_additions'] = max(d['orders'], d['cart_additions'])
-    #d['views'] = max(d['orders'], d['views'])
-
-    #print("d: %s" % d)
-    #print("product: %s" % product)
-    raw_data.update({"date": d['date'], 'productid': d['productid']}, d, upsert=True)
+    raw_data.update({"date": d['date'], 'productid': d['productid'], 'platform': d['platform']}, d, upsert=True)
 
 def preprocess_data():
   print("preprocess_data")
@@ -187,7 +168,7 @@ def preprocess_data():
     print(product)
 #TODO preprocessing 
     p = product
-    processed_data.update({"date": p['date'], "productid": p['productid']}, p, upsert=True)
+    processed_data.update({"date": p['date'], "productid": p['productid'], 'platform': d['platform']}, p, upsert=True)
 
 def normalize(a):
   return (a-min(a))/(max(a)-min(a))
@@ -213,6 +194,12 @@ def calculate_popularity():
     popularity_table.update({"_id": row['productid'], "productid": row['productid']}, row, upsert=True)
   sys.exit()
 
+def post_to_solr():
+  #qwerty
+  #asdf
+  #zxcv
+  #qwer
+
 if argv['fetch_omniture']:
   write_report_data_to_db()
 
@@ -221,3 +208,6 @@ if argv['preprocess']:
 
 if argv['popularity']:
   calculate_popularity()
+
+if argv['post_to_solr']:
+  post_to_solr()
