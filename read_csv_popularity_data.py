69639ae6efdb1756cd5c3a5a153a368eda398cd6
gludonykaa <gludonykaa@gmail.com>
2017-05-17 11:31:54 +0000

    popularity changes

--- read_csv_popularity_data.py
+++ read_csv_popularity_data.py
@@ -6,18 +6,29 @@ raw_data = client['search']['raw_data']
 print(raw_data.find_one())
 
 import csv
-cnt = 0 
-with open('/Users/mayank/Downloads/Product_feed_day_wise/product_data_201606.csv', newline='') as csvfile:
-  spamreader = csv.DictReader(csvfile,)
-  for row in spamreader:
-    cnt +=1 
-    if cnt == 10:
-      break
-    d = dict(row)
-    print(d)
-    date = arrow.get(d['datetime']).datetime
-    filt = {"date": date, "productid": d['name']}
-    update = {"views": int(d["event5"]), "cart_additions": int(d['cartadditions']), "orders": int(d['orders'])}
-    print("filt: %r" % filt)
-    print(update)
-    raw_data.update_one(filt, {"$set": update}, upsert=True) 
+for filename in reversed([
+  '/home/ubuntu/Product_feed_day_wise/product_data_201606.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201607.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201608.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201609.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201610.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201611.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201612.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201701.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201702.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201703.csv',
+  '/home/ubuntu/Product_feed_day_wise/product_data_201704.csv',
+  ]):
+  print(filename)
+  cnt = 0 
+  with open(filename, newline='') as csvfile:
+    spamreader = csv.DictReader(csvfile,)
+    for row in spamreader:
+      cnt +=1 
+      if cnt %1000 == 0 :
+        print("%s rows processed"  % cnt)
+      d = dict(row)
+      date = arrow.get(d['datetime']).datetime
+      filt = {"date": date, "productid": d['name']}
+      update = {"views": int(d["event5"]), "cart_additions": int(d['cartadditions']), "orders": int(d['orders'])}
+      raw_data.update_one(filt, {"$set": update}, upsert=True) 
