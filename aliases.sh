7a024fcf7c7e6ec9a8cdaa81271f6fbb558a694a
gludonykaa <gludonykaa@gmail.com>
2017-07-31 10:49:23 +0000

    aliases

---
+++ aliases.sh
@@ -0,0 +1,14 @@
+alias api='cd /home/apis/nykaa'
+
+alias nykaaproddb="mysql -h dbmaster-qa.cjmplqztt198.ap-southeast-1.rds.amazonaws.com -uapi -paU%v#sq1 -D nykaalive1"
+alias nykaa_analytics=" mysql -h nykaa-analytics.nyk00-int.network -u analytics -pP1u8Sxh7kNr"
+alias nykaapreproddb=" mysql -h preprod-11april2017.cjmplqztt198.ap-southeast-1.rds.amazonaws.com -u nykaalive -poh1ued3phi0uh8ooPh6 -D nykaalive1"
+alias nyscripts='cd /home/ubuntu/nykaa_scripts/'
+alias nyapi='cd /home/apis/nykaa'
+
+alias solrhome='cd /opt/solr/server/scripts/cloud-scripts'
+alias solrup='/opt/solr/server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9983 -cmd upconfig -confdir /home/ubuntu/conf/ -confname autocomplete'
+alias solrdown='/opt/solr/server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9983 -cmd downconfig  -confname autocomplete -confdir /home/ubuntu/conf/'
+alias solrconf='cd /home/ubuntu/conf/'
+alias solrreload='curl "http://localhost:8983/solr/admin/collections?action=RELOAD&name=autocomplete"'
+
