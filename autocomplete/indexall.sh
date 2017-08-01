f8aef41e5aa7391d74d16fcdc49ab9c603049810
Preprod Machine <gludonykaaa@gmail.com>
2017-08-01 07:07:52 +0000

    indexall autocomplete

---
+++ autocomplete/indexall.sh
@@ -0,0 +1,3 @@
+/usr/bin/python3 /nykaa/scripts/autocomplete/indexSearchTerms.py
+/usr/bin/python3 /nykaa/scripts/autocomplete/indexCategories.py
+/usr/bin/python3 /nykaa/scripts/autocomplete/indexBrands.py
