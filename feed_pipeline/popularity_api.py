8d61825b8daeaacb32697b1d3da37636b489909c
gludonykaa <gludonykaa@gmail.com>
2017-08-23 16:14:12 +0000

    popularity_api.py

--- feed_pipeline/popularity_api.py
+++ feed_pipeline/popularity_api.py
@@ -11,6 +11,14 @@ def get_popularity_for_ids(product_ids):
   ret = {obj['_id']:{"popularity":obj['popularity']} for obj in res}
   return ret
 
+def get_views_for_ids(product_ids):
+  assert isinstance(product_ids, list)
+  res = list(popularity_table.find({"_id": {"$in": product_ids}}, {"views":1}))
+
+  ret = {obj['_id']:{"views":obj['views']} for obj in res}
+  return ret
+
 if __name__ == '__main__':
   print(get_popularity_for_ids(['58544', '14862','14927', '7352']))
+  print(get_views_for_ids(['58544', '14862','14927', '7352']))
   
