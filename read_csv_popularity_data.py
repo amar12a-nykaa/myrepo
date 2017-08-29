c8bc53332a41d09a27f5209f87c681c3d06a1360
Admin Machine <msjaiswal@gmail.com>
2017-08-29 10:06:38 +0000

    popularity_upload_date

--- read_csv_popularity_data.py
+++ read_csv_popularity_data.py
@@ -1,3 +1,4 @@
+import sys
 import arrow
 from pymongo import MongoClient
 client = MongoClient()
@@ -7,17 +8,18 @@ print(raw_data.find_one())
 
 import csv
 for filename in reversed([
-  '/home/ubuntu/Product_feed_day_wise/product_data_201606.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201607.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201608.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201609.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201610.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201611.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201612.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201701.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201702.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201703.csv',
-  '/home/ubuntu/Product_feed_day_wise/product_data_201704.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201606.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201607.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201608.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201609.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201610.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201611.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201612.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201701.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201702.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201703.csv',
+#  '/home/ubuntu/Product_feed_day_wise/product_data_201704.csv',
+  '/tmp/gludo_app_20170501-20170823.csv'
   ]):
   print(filename)
   cnt = 0 
@@ -28,7 +30,11 @@ for filename in reversed([
       if cnt %1000 == 0 :
         print("%s rows processed"  % cnt)
       d = dict(row)
-      date = arrow.get(d['datetime']).datetime
-      filt = {"date": date, "productid": d['name']}
-      update = {"views": int(d["event5"]), "cart_additions": int(d['cartadditions']), "orders": int(d['orders'])}
+      print(d)
+      date = arrow.get(d['\ufeffDate'], 'MMM D YYYY').datetime
+      filt = {"date": date, "productid": d['Products']}
+      update = {"views": int(d["Product Views"]), "cart_additions": int(d['Cart Additions']), "orders": int(d['Orders'])}
+      print("--")
+      print(filt)
+      print(update)
       raw_data.update_one(filt, {"$set": update}, upsert=True) 
