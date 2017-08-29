580eaf642190cba8079e85b95b6be8557dd0eb12
Admin Machine <msjaiswal@gmail.com>
2017-08-29 12:24:15 +0000

    popularity viewcount

--- feed_pipeline/popularity_api.py
+++ feed_pipeline/popularity_api.py
@@ -6,19 +6,20 @@ popularity_table = client['search']['popularity']
 
 def get_popularity_for_ids(product_ids):
   assert isinstance(product_ids, list)
-  res = list(popularity_table.find({"_id": {"$in": product_ids}}, {"popularity":1}))
+  res = list(popularity_table.find({"_id": {"$in": product_ids}}))
+  ret = {}
+  for obj in res:
+    if obj['views']:
+      conversion =  1.0 * obj['cart_additions'] / obj['views'] * 100 
+      views = 0 if conversion < 0.05 else conversion
+    else:
+      views = 0
+    ret[obj['_id']] = {"popularity":obj['popularity'], "views": views}
 
-  ret = {obj['_id']:{"popularity":obj['popularity']} for obj in res}
-  return ret
-
-def get_views_for_ids(product_ids):
-  assert isinstance(product_ids, list)
-  res = list(popularity_table.find({"_id": {"$in": product_ids}}, {"views":1}))
-
-  ret = {obj['_id']:{"views":obj['views']} for obj in res}
+  #ret = {obj['_id']:{"popularity":obj['popularity'], "views": views} for obj in res}
   return ret
 
 if __name__ == '__main__':
   print(get_popularity_for_ids(['58544', '14862','14927', '7352']))
-  print(get_views_for_ids(['58544', '14862','14927', '7352']))
+  #print(get_views_for_ids(['58544', '14862','14927', '7352']))
   
