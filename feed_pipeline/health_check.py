1cd08384b6fe10301122e0aaf067d61c1fab79be
gludonykaa <gludonykaa@gmail.com>
2017-08-30 08:06:23 +0000

    health check feed pipeline

--- feed_pipeline/health_check.py
+++ feed_pipeline/health_check.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient 
+import arrow
+import pprint
 
 client = MongoClient()
 
@@ -6,4 +8,21 @@ raw_data = client['search']['raw_data']
 processed_data = client['search']['processed_data']
 popularity = client['search']['popularity']
 
-last_date  = raw_data.aggregate([{"$max":"$date"}])
+res = list(raw_data.aggregate([{"$group": {"_id": "$date", "count": {"$sum": 1}}}, {"$sort": {"_id":1}}]))
+
+dates = {x['_id'] for x in res}
+#print(dates[0:10])
+
+today = arrow.now().replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
+date = today.replace(months=-6)
+all_dates = set()
+while date < today:
+  all_dates.add(date.datetime.replace(tzinfo=None) )
+  date = date.replace(days=1)
+
+missing_dates = all_dates - dates 
+print(dates.pop())
+print(all_dates.pop())
+if len(missing_dates) >= 1:
+  print("Data missing for following dates:")
+  pprint.pprint(missing_dates)
