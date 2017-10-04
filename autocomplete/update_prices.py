27231b4710dfd23865e0fb3218088e50303c7a33
gludonykaa <gludonykaa@gmail.com>
2017-10-04 13:41:20 +0000

    autocomplete: supporting scripts

---
+++ autocomplete/update_prices.py
@@ -0,0 +1,44 @@
+import argparse
+import datetime
+import json
+import os
+import os.path
+import pprint
+import re
+import sys
+import traceback
+from collections import OrderedDict
+from contextlib import closing
+
+import arrow
+import IPython
+import mysql.connector
+import numpy
+import omniture
+import pandas as pd
+from pymongo import MongoClient
+
+sys.path.append("/nykaa/api")
+from pas.v1.utils import Utils
+
+sys.path.append("/nykaa/scripts/utils")
+from omnitureutils import OmnitureUtils
+import requests
+import IPython
+
+client = MongoClient()
+raw_data = client['search']['raw_data']
+processed_data = client['search']['processed_data']
+popularity_table = client['search']['popularity']
+
+
+for p in Utils.mysql_read("select product_id, mrp, sku from products limit 100"):
+  if not p['product_id']:
+    continue
+  print(p)
+  r = requests.get("http://172.30.1.69/apis/v1/pas.get?sku=%s&type=simple" % p['sku'])
+  print(r.text)
+
+  #IPython.embed()
+  #sys.exit()
+
---
+++ autocomplete/update_product_ids.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import argparse
+import json
+import pprint
+import socket
+import sys
+import traceback
+from collections import OrderedDict
+from IPython import embed
+from pymongo import MongoClient
+from datetime import datetime
+from urllib.parse import urlparse
+from urllib.request import Request, urlopen
+
+import dateparser
+import requests
+
+
+sys.path.append("/nykaa/api")
+from pas.v1.csvutils import read_csv_from_file
+from pas.v1.exceptions import SolrError
+from pas.v1.utils import Utils
+
+sys.path.append("/nykaa/scripts/utils")
+from loopcounter import LoopCounter
+
+
+client = MongoClient()
+master_feed= client['feed_pipeline']['master_feed']
+
+conn =  Utils.mysqlConnection()
+conn_nykaa =  Utils.nykaaMysqlConnection()
+#embed()
+
+ctr = LoopCounter(name='Updating product_id and parent_id', total=master_feed.count())
+for doc in master_feed.find({}, {"product_id": 1, "parent_id": 1, "sku": 1}).limit(0):
+	try:
+		ctr += 1
+		if ctr.should_print():
+			print(ctr.summary)
+
+		set_clause_arr = []
+		#if doc.get('parent_id'):
+		#	set_clause_arr.append("parent_id='%s'" % doc.get('parent_id'))
+		if doc.get('product_id'):
+			set_clause_arr.append("product_id='%s'" % doc.get('product_id'))
+
+		if set_clause_arr:
+			set_clause = " set " + ", ".join(set_clause_arr)
+			query = "update products {set_clause} where sku ='{sku}' ".format(set_clause=set_clause, sku=doc['sku'])
+			#print(query)
+			Utils.mysql_write(query, connection=conn)
+	except:
+		print(traceback.format_exc())
+
