516e2b8779c33cd695dd5523c87bb18c57018f7e
Preprod Machine <gludonykaaa@gmail.com>
2017-10-05 17:35:07 +0000

    minor change

--- feed_pipeline/commands.sh
+++ feed_pipeline/commands.sh
@@ -4,3 +4,9 @@ mongoexport --db search --collection raw_data --out /tmp/raw_data.json
 mongoimport --db search --collection popularity --file /tmp/popularity.json
 
  db.raw_data.update({date:{$gte: ISODate("2017-05-01T00:00:00Z")}}, {$set: {platform: "app"}}, {multi:true})
+
+# MongoDump DB
+mongodump --gzip --db search --archive=/tmp/search.gz 
+scpny /tmp/search.gz ubuntu@admin:/tmp/search.gz
+# MongoRestore DB
+mongorestore --gzip --db search --archive=/tmp/search.gz 
