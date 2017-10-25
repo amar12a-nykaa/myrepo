b76a3a3e12c7c116c388c6894ed3f23484a62619
Admin Machine <msjaiswal@gmail.com>
2017-10-25 12:30:04 +0000

    autocomplete: minor fix

--- autocomplete/index.py
+++ autocomplete/index.py
@@ -45,7 +45,9 @@ def create_map_search_product():
   map_search_product = {}
 
   for query in [p['query'] for p in search_terms_normalized.find()]:
-    base_url = "http://localhost:8983/solr/yang/select"
+    base_url = Utils.solrHostName() + "/solr/yang/select"
+    #embed()
+    #exit()
     f = furl(base_url) 
     f.args['defType'] = 'dismax'
     f.args['indent'] = 'on'
@@ -291,7 +293,7 @@ def index_all(collection):
 def fetch_product_by_parentid(parent_id):
   DEBUG = False 
   product = {}
-  base_url = "http://localhost:8983/solr/yang/select"
+  base_url = Utils.solrHostName() + "/solr/yang/select"
   f = furl(base_url) 
   f.args['q'] = 'parent_id:%s AND product_id:%s' % (parent_id,parent_id)
   f.args['fq'] = 'type:"simple" OR type:"configurable"'
