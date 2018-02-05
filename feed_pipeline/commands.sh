045003f34887a414cf5f92bf21ea66ec1f619bf6
Preprod Machine <gludonykaaa@gmail.com>
2018-02-05 08:17:21 +0000

    feed_pipeline commands

--- feed_pipeline/commands.sh
+++ feed_pipeline/commands.sh
@@ -1,9 +1,14 @@
 # MongoExport Data
 mongoexport --db search --collection raw_data --out /tmp/raw_data.json
+mongoexport --db search --collection popularity --out /tmp/popularity.json
+scpny /tmp/popularity.json ubuntu@admin:/tmp/popularity.json
+
 # Mongo Import Data
+mongoimport --db search --collection raw_data --file /tmp/raw_data.json
 mongoimport --db search --collection popularity --file /tmp/popularity.json
 
- db.raw_data.update({date:{$gte: ISODate("2017-05-01T00:00:00Z")}}, {$set: {platform: "app"}}, {multi:true})
+# 
+db.raw_data.update({date:{$gte: ISODate("2017-05-01T00:00:00Z")}}, {$set: {platform: "app"}}, {multi:true})
 
 # MongoDump DB
 mongodump --gzip --db search --archive=/tmp/search.gz 
