fb2e9c416b38153453173aa02db3d465578490af
AshleshaGawade <ashlesha.gawade@nykaa.com>
2018-02-17 14:20:25 +0530

    d_sku changes in script

--- feed_pipeline/indexCatalog.py
+++ feed_pipeline/indexCatalog.py
@@ -203,6 +203,12 @@ class CatalogIndexer:
         if row['product_expiry']:
           doc['expiry_date'] = dateparser.parse(row['product_expiry'], ['%d-%m-%Y %H:%M', '%Y-%m-%d %H:%M:%S']).strftime('%Y-%m-%dT%H:%M:%SZ')
         doc['is_service'] = row.get('d_sku') == '1'
+        if(row['d_sku'] == 2):
+          doc['d_sku_s'] = 'giftcard'
+        elif(row['d_sku'] == 1):
+          doc['d_sku_s'] = 'beauty_service'
+        else:
+          doc['d_sku_s'] = 'none'
         doc['pack_size'] = row['pack_size']
         doc['is_luxe'] = row.get('is_luxe') == '1'
         doc['can_subscribe'] = row.get('is_subscribable') == '1'
