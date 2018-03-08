727dd4e76fb814861c338a020d508a013fff992c
QA Machine <gludonykaaa@gmail.com>
2018-03-08 23:52:06 +0000

    feedpipeline points to v2

--- feed_pipeline/catalogPipeline.py
+++ feed_pipeline/catalogPipeline.py
@@ -17,7 +17,7 @@ from esutils import EsUtils
 from importDataFromNykaa import NykaaImporter
 from indexCatalog import CatalogIndexer
 sys.path.append('/home/apis/nykaa/')
-from pas.v1.utils import Utils, CATALOG_COLLECTION_ALIAS
+from pas.v2.utils import Utils, CATALOG_COLLECTION_ALIAS
 
 def indexSolrData(file_path, force_run):
   collections = SolrUtils.get_active_inactive_collections(CATALOG_COLLECTION_ALIAS)
@@ -153,11 +153,11 @@ import_stop = timeit.default_timer()
 import_duration = import_stop - import_start
 print("Time taken to import data from Nykaa: %s seconds" % time.strftime("%M min %S seconds", time.gmtime(import_duration)))
 
-# Index Solr Data
-indexSolrData(file_path, force_run)
-
 # Index Elastic Search Data
 indexESData(file_path, force_run)
+
+# Index Solr Data
+indexSolrData(file_path, force_run)
 
 script_stop = timeit.default_timer()
 script_duration = script_stop - script_start
