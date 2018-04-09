d3a7384eeb9c2ceaee5642a2d113d3d81895d377
QA Machine <gludonykaaa@gmail.com>
2018-04-09 09:00:59 +0000

    autocomplete cleanup - mayank

--- autocomplete/obsolete/indexall.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/python3 /nykaa/scripts/autocomplete/indexSearchTerms.py
-/usr/bin/python3 /nykaa/scripts/autocomplete/indexCategories.py
-/usr/bin/python3 /nykaa/scripts/autocomplete/indexBrands.py
