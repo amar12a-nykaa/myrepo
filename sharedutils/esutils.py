262530d25b4838c1ec735842b90e74add5929c5f
Preprod Machine <gludonykaaa@gmail.com>
2018-07-25 13:03:00 +0000

    Automatic creation of indexes and alias for entity - mayank

--- sharedutils/esutils.py
+++ sharedutils/esutils.py
@@ -52,36 +52,37 @@ class EsUtils:
 
   def get_index_from_alias(alias):
     response = {}
-    try:
-      es = Utils.esConn()
-      if es.indices.exists_alias(alias):
-        response = es.indices.get_alias(
-          index=alias
-        )
-      else:
-        if index_alias_config.get(alias):
-          settings = index_alias_config.get(alias)
-          if settings and settings.get('collections'):
-            index_client =  EsUtils.get_index_client()
-            for index in settings['collections']:
-              if not index_client.exists(index):
-                if index in ['yin', 'yang']:
-                  schema = json.load(open('/home/ubuntu/nykaa_scripts/feed_pipeline/schema.json'))
-                  es.indices.create(index = index, body = schema)
-                  es.indices.put_alias(index= index, name = alias)
-                if index in ['autocomplete_yin', 'autocomplete_yang']:
-                  schema = json.load(open('/home/ubuntu/nykaa_scripts/autocomplete/schema.json'))
-                  es.indices.create(index = index, body = schema)
-                  es.indices.put_alias(index= index, name = alias)
-              else:
+    es = Utils.esConn()
+    if es.indices.exists_alias(alias):
+      response = es.indices.get_alias(
+        index=alias
+      )
+    else:
+      if index_alias_config.get(alias):
+        settings = index_alias_config.get(alias)
+        if settings and settings.get('collections'):
+          index_client =  EsUtils.get_index_client()
+          for index in settings['collections']:
+            if not index_client.exists(index):
+              if index in ['yin', 'yang']:
+                schema = json.load(open('/home/ubuntu/nykaa_scripts/feed_pipeline/schema.json'))
+                es.indices.create(index = index, body = schema)
                 es.indices.put_alias(index= index, name = alias)
+              if index in ['autocomplete_yin', 'autocomplete_yang']:
+                schema = json.load(open('/home/ubuntu/nykaa_scripts/autocomplete/schema.json'))
+                es.indices.create(index = index, body = schema)
+                es.indices.put_alias(index= index, name = alias)
+              if index in ['entity_yin', 'entity_yang']:
+                schema = json.load(open('/home/ubuntu/nykaa_scripts/feed_pipeline/entity_schema.json'))
+                es.indices.create(index = index, body = schema)
+                es.indices.put_alias(index= index, name = alias)
+            else:
+              es.indices.put_alias(index= index, name = alias)
 
-      for index, index_aliases in response.items():
-        return index
-    except Exception as e:
-      print(traceback.format_exc())
-      raise
-    return response
+    for index, index_aliases in response.items():
+      return index
+
+    raise Exception("Couldnt find index for alias: %s" % alias )
 
   def get_active_inactive_indexes(index_alias):
     for alias, settings in index_alias_config.items():
@@ -91,7 +92,7 @@ class EsUtils:
           if index != active_index:
             return {'active_index'  : active_index, 'inactive_index' : index}
 
-    return None
+    raise Exception("Cannot find alias: %s" % index_alias)
 
   def switch_index_alias(alias, from_index, to_index):
     response = {}
