7c98d4e92676961a0cf9c60ded076dd015e0e2d9
QA Machine <gludonykaaa@gmail.com>
2018-08-07 16:56:51 +0000

    autocomplete exclude pro- mayank

--- autocomplete/index.py
+++ autocomplete/index.py
@@ -358,7 +358,7 @@ def index_category_facets(collection, searchengine):
   index_docs(searchengine, docs, collection)
 
 
-def validate_min_count(force_run):
+def validate_min_count(force_run, allowed_min_docs):
   #force_run = False
 
   indexes = EsUtils.get_active_inactive_indexes('autocomplete')
@@ -382,6 +382,9 @@ def validate_min_count(force_run):
   else:
     docs_ratio = num_docs_inactive/num_docs_active
   if docs_ratio < 0.95 and not force_run:
+    if allowed_min_docs > 0 and  num_docs_inactive > allowed_min_docs:
+      print("Validation is OK since num_docs_inactive > allowed_min_docs")
+      return
     msg = "[ERROR] Number of documents decreased by more than 5% of current documents. Please verify the data or run with --force option to force run the indexing."
     print(msg)
     raise Exception(msg)
@@ -549,7 +552,7 @@ def fetch_product_by_parentids(parent_ids):
   return final_docs
 
 
-def index_engine(engine, collection=None, active=None, inactive=None, swap=False, index_search_queries_arg=False, index_products_arg=False, index_categories_arg=False, index_brands_arg=False,index_brands_categories_arg=False, index_category_facets_arg=False, index_all=False, force_run=False ):
+def index_engine(engine, collection=None, active=None, inactive=None, swap=False, index_search_queries_arg=False, index_products_arg=False, index_categories_arg=False, index_brands_arg=False,index_brands_categories_arg=False, index_category_facets_arg=False, index_all=False, force_run=False, allowed_min_docs=0 ):
     assert len([x for x in [collection, active, inactive] if x]) == 1, "Only one of the following should be true"
 
     if index_all:
@@ -613,7 +616,7 @@ def index_engine(engine, collection=None, active=None, inactive=None, swap=False
     
     if swap:
       print("Swapping Index")
-      validate_min_count(force_run)
+      validate_min_count(force_run=force_run, allowed_min_docs=allowed_min_docs)
       indexes = EngineUtils.get_active_inactive_indexes('autocomplete')
       EngineUtils.switch_index_alias('autocomplete', indexes['active_index'], indexes['inactive_index'])
 
@@ -636,6 +639,7 @@ if __name__ == '__main__':
   parser.add_argument("--buildonly", action='store_true', help="Build Suggester")
   parser.add_argument("--fast", action='store_true', help="Index a fraction of products and search queries to save on indexing time")
   parser.add_argument("--force", action='store_true', help="Ignore Validation")
+  parser.add_argument("--allowed_min_docs", type=int, default=0, help="Minimum number of docs allowed")
 
   collection_state = parser.add_mutually_exclusive_group(required=True)
   collection_state.add_argument("--inactive", action='store_true')
@@ -652,6 +656,6 @@ if __name__ == '__main__':
   index_all = not any([argv[x] for x in required_args]) and not argv['buildonly']
 
   startts = time.time()
-  index_engine(engine='elasticsearch', collection=argv['collection'], active=argv['active'], inactive=argv['inactive'], swap=argv['swap'], index_products_arg=argv['product'], index_search_queries_arg=argv['search_query'], index_categories_arg=argv['category'], index_brands_arg=argv['brand'], index_brands_categories_arg=argv['brand_category'], index_category_facets_arg=argv['category_facet'],index_all=index_all, force_run=argv['force'])
+  index_engine(engine='elasticsearch', collection=argv['collection'], active=argv['active'], inactive=argv['inactive'], swap=argv['swap'], index_products_arg=argv['product'], index_search_queries_arg=argv['search_query'], index_categories_arg=argv['category'], index_brands_arg=argv['brand'], index_brands_categories_arg=argv['brand_category'], index_category_facets_arg=argv['category_facet'],index_all=index_all, force_run=argv['force'], allowed_min_docs=argv['allowed_min_docs'])
   mins = round((time.time()-startts)/60, 2)
   print("Time taken: %s mins" % mins)
