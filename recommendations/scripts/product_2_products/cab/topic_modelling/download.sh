b42a28ae037f2f8857955aadb936d3ce800f9025
root <ashwin@gludo.com>
2018-09-11 08:53:01 +0000

    Adding topic modelling code for generating recommendations

---
+++ recommendations/scripts/product_2_products/cab/topic_modelling/download.sh
@@ -0,0 +1,7 @@
+ #!/bin/bash
+sudo python3 -m pip install boto3
+sudo python3 -m pip install pandas
+sudo python3 -m pip install argparse
+sudo python3 -m pip install gensim
+sudo python3 -m pip install smart_open
+sudo python3 -m pip install s3fs
