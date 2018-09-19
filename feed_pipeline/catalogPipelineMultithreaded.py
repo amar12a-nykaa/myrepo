d935bbaaafffb0ff2497267a9e14e832dbca1740
anoopnykaa <anoop.singh@nykaa.com>
2018-09-19 12:06:52 +0530

    changes to refresh index

--- feed_pipeline/catalogPipelineMultithreaded.py
+++ feed_pipeline/catalogPipelineMultithreaded.py
@@ -73,7 +73,7 @@ def indexESData(file_path, force_run):
 
     sett = {'refresh_interval': '1s'}
     index_client.put_settings(sett, inactive_index)
-    time.sleep(3)
+    index_client.refresh(index=inactive_index)
     index_stop = timeit.default_timer()
     index_duration = index_stop - index_start
     print("Time taken to index data to ElasticSearch: %s seconds" % time.strftime("%M min %S seconds",
