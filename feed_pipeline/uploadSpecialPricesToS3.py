07b61d8f162ccd05f24ec27695a0fd333f7edaf0
kedar-nykaa <kedar.pagdhare@nykaa.com>
2018-10-20 12:19:24 +0530

    date based csv name

--- feed_pipeline/uploadSpecialPricesToS3.py
+++ feed_pipeline/uploadSpecialPricesToS3.py
@@ -1,6 +1,9 @@
 import argparse
 import sys
 import os
+import pytz
+import datetime
+from decimal import ROUND_HALF_UP, Decimal
 
 sys.path.append('/home/apis/nykaa/')
 
@@ -9,10 +12,11 @@ from contextlib import closing
 
 
 def upload_special_price_to_s3(batch_size = 1000):
-
-  file_name = 'special_price.csv'
+  tz = pytz.timezone('Asia/Kolkata')
+  local_date = datetime.datetime.now(tz=tz).strftime('%d-%m-%Y')
+  file_name = 'special_price_{}.csv'.format(local_date)
   f = open(file_name, "w")
-  query = "SELECT sku, sp, type FROM products;"
+  query = "SELECT sku, mrp, discount, msp, type FROM products;"
   connection = Utils.mysqlConnection()
   with closing(connection.cursor()) as cursor:
     cursor.execute(query)
@@ -22,7 +26,8 @@ def upload_special_price_to_s3(batch_size = 1000):
       if not results:
         break
       for result in results:
-        line = '"{}", "{}", "{}"\n'.format(result[0], result[1], result[2])
+        special_price = calculate_sp(mrp=result[1], discount=result[2], msp=result[3])
+        line = '"{}", "{}", "{}"\n'.format(result[0], special_price, result[4])
         print(line)
         f.write(line)
 
@@ -50,6 +55,15 @@ def upload_special_price_to_s3(batch_size = 1000):
   f.close()
   Utils.upload_file_to_s3(file_name)
   os.remove(file_name)
+
+
+def calculate_sp(mrp, discount, msp):
+  if discount is None:
+    return msp
+  sp = Decimal(str(mrp)) - Decimal(str(mrp)) * Decimal(str(discount)) / Decimal(str(100))
+  if msp:
+    sp = min(Decimal(str(msp)), sp)
+  return float(sp.quantize(0, ROUND_HALF_UP))
 
 
 if __name__ == "__main__":
