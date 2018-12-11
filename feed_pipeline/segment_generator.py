1420707d436f87fe34c292e6c26f83d72de007dc
Preprod Machine <gludonykaaa@gmail.com>
2018-12-11 09:55:31 +0000

    Script to migrate user's data from redshift to Dynamodb

--- feed_pipeline/segment_generator.py
+++ feed_pipeline/segment_generator.py
@@ -10,43 +10,50 @@ sys.path.append('/home/apis/nykaa/')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nykaa.settings')
 django.setup()
 from pas.v2.utils import Utils
-from pas.v2.utils import DyanamoDbTable
+from pas.v2.utils import UserProfileServiceDynamoDb 
 from pas.v2.models import UserDataStore 
 from contextlib import closing
+sys.path.append('/nykaa/scripts/sharedutils/')
+from loopcounter import LoopCounter
 
-def updateDyanamodb(row):
+def updateDyanamodb(row,db):
   data = {}
-  data["userId"] = str(row[0])
+  data["user_id"] = str(row[0])
   if row[1]:
-      data["firstName"] = row[1]
+      data["first_name"] = row[1]
   if row[2]:
-      data["lastName"] = row[2]
+      data["last_name"] = row[2]
   if row[3]:
       data["gender"] = row[3]
   if row[4]:
       data["dob"] = str(row[4])
   if row[5]:
       data["email"] = row[5]
-  UserDataStore.update_user_data(user_id=data["userId"],new_attributes=data)
+  db.put_item(item=data)    
+  #UserDataStore.update_user_data(user_id=data["user_id"],new_attributes=data)
 
 
 
 
-db = DyanamoDbTable(table_name='NykaaUserData', region_name = 'ap-southeast-1')
+db = UserProfileServiceDynamoDb()
 connection = Utils.redshiftConnection()
 #cursor= connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 query = """
-              select customer_id, firstname, lastname, gender, dob, email FROM customer_info 
-        """
+            select customer_id, firstname, lastname, gender, dob, email FROM customer_info c JOIN customer_view v ON c.customer_id=v.id WHERE v.order_total_at_mrp > 0        """
 rows = []
+print("Starting the script")
 with closing(connection.cursor()) as cursor:
   cursor.execute(query)
+  ctr = LoopCounter(name='Segment Generator')
   while True:
     batch_empty = True
-    for row in cursor.fetchmany(10):
+    for row in cursor.fetchmany(10000):
+      ctr += 1
+      if ctr.should_print():
+        print(ctr.summary)
       batch_empty = False
       rows.append(row)
-      updateDyanamodb(row)
+      updateDyanamodb(row,db)
     if batch_empty:
       break
 
