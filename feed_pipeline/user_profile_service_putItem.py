c490383289d018b9eac83b1f55cc78fd388ea0f2
Preprod Machine <gludonykaaa@gmail.com>
2018-12-21 10:25:36 +0000

    read and upload data daily from s3

--- feed_pipeline/user_profile_service_putItem.py
+++ feed_pipeline/user_profile_service_putItem.py
@@ -2,38 +2,49 @@ import csv
 import json
 import sys
 import boto3
+import pandas as pd
+import subprocess
 sys.path.append('/home/apis/nykaa/')
 from pas.v2.utils import Utils
 from pas.v2.utils import UserProfileServiceDynamoDb
+from os import listdir
+from datetime import datetime, timedelta
 
-csvfile = open('Samplecsv.csv', 'r')
+yesterday = datetime.now() - timedelta(days=1)
+foldername = yesterday.strftime('%Y%m%d')
 
-header = csvfile.readline()
-header = header.rstrip("\n")
-columns = header.split(",")
+cmd = "aws s3 sync "+"s3://nykaa-ups/daily_data/"+foldername+" "+"/home/ubuntu/nykaa_scripts/feed_pipeline/data/"+foldername+"/"
+out = subprocess.check_output(cmd , shell=True).strip()
+if out:
+    print(out)
 
-fieldnames = (columns)
-reader = csv.DictReader( csvfile, fieldnames)
+files = [f for f in listdir("/home/ubuntu/nykaa_scripts/feed_pipeline/data/"+foldername)]
+print(files)
+
 db = boto3.resource('dynamodb')
 table = db.Table('user_profile_service')
-filename = "privy"
 
-
-with open('Samplecsv.csv') as csv_file:
-    csvfile.readline()
+for filename in files:
+  csvfile = open("/home/ubuntu/nykaa_scripts/feed_pipeline/data/"+foldername+"/"+filename, 'r')
+  header = csvfile.readline()
+  header = header.rstrip("\n")
+  columns = header.split(",")
+  fileName = filename
+  attribute_name = fileName.split("__")
+  attribute = attribute_name[0]
+  with open("/home/ubuntu/nykaa_scripts/feed_pipeline/data/"+foldername+"/"+filename) as csv_file:
     for user in csv.DictReader(csv_file):
-        new_data = {}
-        for column in range(1,len(columns)):
-            new_data[columns[column]] = user[columns[column]]
-        user_data = table.get_item(Key={'user_id':user['customer_id']})
-        if 'Item' in user_data.keys() and 'privy' in user_data['Item'].keys():
-          privy_data = user_data['Item']['privy']
-        else:
-          privy_data={}
-        for key in new_data.keys():
-            privy_data[key]=new_data[key]
-        if 'Item' in user_data.keys():
-          table.update_item(Key={'user_id':  user_data['Item']['user_id']},AttributeUpdates={"privy":{"Action": "PUT","Value":privy_data}})  
-        else:
-          table.put_item(Item = {'user_id':  user['customer_id'],'privy':privy_data})
-
+      new_data = {}
+      for column in range(1,len(columns)):
+        new_data[columns[column]] = user[columns[column]]
+      user_data = table.get_item(Key={'user_id':user['customer_id']})
+      if 'Item' in user_data.keys() and attribute in user_data['Item'].keys():
+        privy_data = user_data['Item'][attribute]
+      else:
+        privy_data={}
+      for key in new_data.keys():
+        privy_data[key]=new_data[key]
+      if 'Item' in user_data.keys():
+        table.update_item(Key={'user_id':  user_data['Item']['user_id']},AttributeUpdates={attribute:{"Action": "PUT","Value":privy_data}})
+      else:
+        table.put_item(Item = {'user_id':  user['customer_id'],attribute:privy_data})
