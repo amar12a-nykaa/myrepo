3a3b5996b719c0c30c14f4d60b3be74fa7721369
root <ashwin@gludo.com>
2019-01-09 10:13:30 +0000

    Topic Modelling EMR added run.sh script for different env

--- recommendations/scripts/common/topic_modelling/topic_modelling_download.sh
+++ recommendations/scripts/common/topic_modelling/topic_modelling_download.sh
@@ -5,3 +5,7 @@ sudo python3 -m pip install argparse
 sudo python3 -m pip install gensim
 sudo python3 -m pip install smart_open
 sudo python3 -m pip install s3fs
+sudo python3 -m pip install psycopg2
+sudo python3 -m pip install mysqlclient
+sudo python3 -m pip install mysql-connector-python
+sudo python3 -m pip install elasticsearch
