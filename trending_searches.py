8da2ccbc81501f47287eb5a6c68942eef19a162c
niharika <niharika.vallambatta@nykaa.com>
2019-02-27 12:26:12 +0530

    changes in trending_searches
    Signed-off-by: niharika <niharika.vallambatta@nykaa.com>

--- trending_searches.py
+++ trending_searches.py
@@ -75,7 +75,7 @@ def group_filter(x):
 def popular_searches(df, df_new):
     dftemp = df[0:0]
     for i in range(len(df.index)):
-        if (df_new.iloc[i, 2] * 100) / (df.iloc[i, 2] - df_new.iloc[i, 2]) > 30:
+        if (df.iloc[i]['frequency'] - df_new.iloc[i]['frequency'])!=0 and (df_new.iloc[i]['frequency']) / (df.iloc[i]['frequency'] - df_new.iloc[i]['frequency']) > 0.8:
             dftemp = dftemp.append(df.iloc[i], ignore_index=True)
     return dftemp
 
@@ -95,10 +95,7 @@ def get_trending_searches():
     df['cleaned_term'] = df['internal_search_term'].map(word_clean)
     idx = df.groupby(['cleaned_term'])['frequency'].transform(max) == df['frequency']
     temp = df[idx]
-    temp.drop(['frequency'], axis=1, inplace=True)
-    temp.drop(['click_interaction_instance'], axis=1, inplace=True)
-    temp.drop(['date'], axis=1, inplace=True)
-
+    temp.drop(['frequency','click_interaction_instance','date'], axis=1, inplace=True)
 
     # grouping all the exact matched terms on same date with aggregation on freq,ctr
     df = df.groupby(['cleaned_term', 'date'], as_index=False).agg({'frequency': 'sum',
@@ -113,44 +110,32 @@ def get_trending_searches():
     # print(df)
     df_new = pd.DataFrame
     df_new = df.ix[df.date == previous]
-    df_new.drop(['date'], axis=1, inplace=True)
-    df_new = df_new.sort_values(['cleaned_term'])
 
     df = df.groupby(['cleaned_term','internal_search_term'], as_index=False).agg({'frequency': 'sum',
                                                            'click_interaction_instance': 'sum'})
-    print(df)
-    df = df.sort_values(['cleaned_term'])
-    # print(df.columns)
-    # print(df_new.columns)
+
     # top 3 terms which are suddenly into popular list
     data = pd.DataFrame
     data = popular_searches(df, df_new)
-    # print(data.head(5))
-    # data.columns = ['cleaned_term','internal_search_term','frequency','click_interaction_instance']
-    data.drop(data[data.frequency < 100].index, inplace=True)
 
-    # add column for CTR
-    # print(data.head(5))
-    data['CTR'] = data[['frequency', 'click_interaction_instance']].apply(ctr_calc, axis=1)
-    data.drop(data[data.CTR < 30].index, inplace=True)
+    # delete rows whose CTR < 30%
+    #data.drop(data[(data.frequency // data.click_interaction_instance) < 0.3].index, inplace=True)
     data = data.sort_values(['frequency', 'click_interaction_instance'], ascending=False)
     data = data.head(5)
-    data.drop(['CTR'], axis=1, inplace=True)
 
+    print (data)
     # top 3 frequently searched terms
-    df = df[df.frequency > 500]
     df = df.sort_values(['frequency', 'click_interaction_instance'], ascending=False)
     df = df.head(3)
 
     return data
-
 
 def insert_trending_searches(data):
     mysql_conn = Utils.mysqlConnection('w')
     cursor = mysql_conn.cursor()
 
     if not Utils.mysql_read("SHOW TABLES LIKE 'trending_searches'", connection=mysql_conn):
-        Utils.mysql_write("create table trending_searches(type varchar(64),url varchar(255),q varchar(255))",
+        Utils.mysql_write("create table trending_searches(type VARCHAR(64) DEFAULT 'query',url VARCHAR(255),q VARCHAR(255))",
                           connection=mysql_conn)
     Utils.mysql_write("delete from trending_searches", connection=mysql_conn)
 
@@ -162,8 +147,8 @@ def insert_trending_searches(data):
         word = " ".join(ls)
         url = "/search/result/?q=" + word.replace(" ", "+")
         print(url)
-        values = ('query', url, word)
-        query = """INSERT INTO trending_searches (type, url,q) VALUES ('%s', '%s','%s') """ % (values)
+        values = (url, word)
+        query = """INSERT INTO trending_searches ( url,q) VALUES ('%s','%s') """ % (values)
 
         cursor.execute(query)
         mysql_conn.commit()
@@ -176,4 +161,4 @@ if __name__ == '__main__':
     data = pd.DataFrame
     data = get_trending_searches()
 
-    insert_trending_searches(data)
+    #insert_trending_searches(data)
