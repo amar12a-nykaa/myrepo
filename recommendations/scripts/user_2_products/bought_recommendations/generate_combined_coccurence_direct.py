bad5897dbf75a149cc714e1f65be224720c76e8f
Ubuntu <ashwin@gludo.com>
2019-05-04 08:05:27 +0000

    Made script completely parallel with pyspark

--- recommendations/scripts/user_2_products/bought_recommendations/generate_combined_coccurence_direct.py
+++ recommendations/scripts/user_2_products/bought_recommendations/generate_combined_coccurence_direct.py
@@ -91,6 +91,32 @@ class Utils:
                     print("MySQL connection failed 3 times. Giving up..")
                     raise
 
+    @staticmethod
+    def mlMysqlConnection(env, connection_details=None):
+        if env == 'prod':
+            host = "ml-db-master.nykaa-internal.com"
+            password = 'fjxcneXnq2gptsTs'
+        elif env in ['non_prod', 'preprod', 'qa']:
+            host = 'nka-preprod-ml.cjmplqztt198.ap-southeast-1.rds.amazonaws.com'
+            password = 'JKaPHGB4JWXM'
+        else:
+            raise Exception('Unknow env')
+        user = 'ml'
+        db = 'nykaa_ml'
+        for i in [0, 1, 2]:
+            try:
+                if connection_details is not None and isinstance(connection_details, dict):
+                    connection_details['host'] = host
+                    connection_details['user'] = user
+                    connection_details['password'] = password
+                    connection_details['database'] = db
+                return mysql.connector.connect(host=host, user=user, password=password, database=db)
+            except:
+                print("MySQL connection failed! Retyring %d.." % i)
+                if i == 2:
+                    print(traceback.format_exc())
+                    print("MySQL connection failed 3 times. Giving up..")
+                    raise
 
     @staticmethod
     def esConn(env):
@@ -283,7 +309,6 @@ def compute_recommendations(env, algo, platform, computation_start_datetime, p2p
 
     #customer_2_products_purchased = {row['customer_id']: row['products_purchased'] for row in _df.groupby('customer_id').agg(func.collect_list('product_id').alias('products_purchased')).collect()}
 
-    #embed()
     df = df.withColumnRenamed('product_id', 'product_id_x').join(df.withColumnRenamed('product_id', 'product_id_y'), on="customer_id", how="inner")
     #df = df.select(['product_id_x', 'product_id_y', 'customer_id']).distinct()
     df = df[df.product_id_x < df.product_id_y]
@@ -315,16 +340,17 @@ def compute_recommendations(env, algo, platform, computation_start_datetime, p2p
     compute_similarity_udf = udf(compute_similarity, FloatType())
     print('Computing similarity')
     df = df.withColumn("similarity", compute_similarity_udf(df['customers_intersection'], df['customers_union']))
+    df = df.withColumnRenamed('product_id_x', 'product').withColumnRenamed('product_id_y', 'recommendation').select(['product', 'recommendation', 'similarity']).union(df.withColumnRenamed('product_id_y', 'product').withColumnRenamed('product_id_x', 'recommendation').select(['product', 'recommendation', 'similarity'])).select(['product', 'recommendation', 'similarity']).withColumn('rank', func.dense_rank().over(Window.partitionBy('product').orderBy(desc('similarity')))).filter(col('rank') < 100)
 
     #df = df.withColumnRenamed('product_id_x', 'product').withColumnRenamed('product_id_y', 'recommendation').union(df.withColumnRenamed('product_id_y', 'product').withColumnRenamed('product_id_x', 'recommendation'))
 
-    direct_similar_products_dict = defaultdict(lambda: [])
+#    direct_similar_products_dict = defaultdict(lambda: [])
 
-    for row in df.collect():
-        if (row['is_luxe_x'] and row['is_luxe_y'])  or not row['is_luxe_x']:
-            direct_similar_products_dict[row['product_id_x']].append((row['product_id_y'], row['similarity']))
-        if (row['is_luxe_y'] and row['is_luxe_x'])  or not row['is_luxe_y']:
-            direct_similar_products_dict[row['product_id_y']].append((row['product_id_x'], row['similarity']))
+#    for row in df.collect():
+#        if (row['is_luxe_x'] and row['is_luxe_y'])  or not row['is_luxe_x']:
+#            direct_similar_products_dict[row['product_id_x']].append((row['product_id_y'], row['similarity']))
+#        if (row['is_luxe_y'] and row['is_luxe_x'])  or not row['is_luxe_y']:
+#            direct_similar_products_dict[row['product_id_y']].append((row['product_id_x'], row['similarity']))
 
 
     rows = []
@@ -333,47 +359,34 @@ def compute_recommendations(env, algo, platform, computation_start_datetime, p2p
     if customer_id:
         c2p_df = c2p_df.filter(col('customer_id') == customer_id)
 
-    customer_2_products_purchased = {row['customer_id']: row['products'] for row in c2p_df.select(['customer_id', 'order_id', 'product_id', 'order_date']).distinct().withColumn('rank', func.dense_rank().over(Window.partitionBy('customer_id').orderBy(desc('order_date')))).filter(col('rank') <= orders_count).groupBy('customer_id').agg(func.collect_list('product_id').alias('products')).collect()}
+#    customer_2_products_purchased = {row['customer_id']: row['products'] for row in c2p_df.select(['customer_id', 'order_id', 'product_id', 'order_date']).distinct().withColumn('rank', func.dense_rank().over(Window.partitionBy('customer_id').orderBy(desc('order_date')))).filter(col('rank') <= orders_count).groupBy('customer_id').agg(func.collect_list('product_id').alias('products')).collect()}
 
-    customer_ids = list(customer_2_products_purchased.keys())
     if limit:
+        customer_ids = list(c2p_df.agg(func.collect_set('customer_id')).collect()[0]['collect_set(customer_id)'])
         customer_ids = customer_ids[:limit]
 
-    customer_ids_chunks = [customer_ids[i:i+100] for i in range(0, len(customer_ids), 100)]
-    Parallel(n_jobs=30, verbose=1, pre_dispatch='1.5*n_jobs', backend="threading")(delayed(compute_recommendation_rows)(customer_ids_chunk, 'user', 'bought', algo, customer_2_products_purchased, rows, direct_similar_products_dict) for customer_ids_chunk in customer_ids_chunks)
 
-#        similar_products = []
-#        for product_purchased in products_purchased:
-#            similar_products += direct_similar_products_dict[product_purchased]
-#        similar_products_dict = defaultdict(lambda: 0)
-#        for p in similar_products:
-#            similar_products_dict[p[0]] += p[1]
-#        direct_similar_products = list(map(lambda e: int(e[0]), sorted(similar_products_dict.items(), key=lambda e: e[1], reverse=True)))
-#        direct_similar_products = list(filter(lambda x: x not in products_purchased, direct_similar_products))[:200]
-#        rows.append((platform, customer_id, 'user', 'bought', algo, json.dumps(direct_similar_products)))
+    c2p_df = c2p_df.filter(col('customer_id').isin(customer_ids)).select(['customer_id', 'order_id', 'product_id', 'order_date']).distinct().withColumn('rank', func.dense_rank().over(Window.partitionBy('customer_id').orderBy(desc('order_date')))).filter(col('rank') <= orders_count).select(['customer_id', 'product_id']).distinct().withColumnRenamed('product_id', 'product')
+
+    c2r_df = c2p_df.join(df, on='product', how='inner').groupBy(['customer_id', 'recommendation']).agg({'similarity': 'avg'}).withColumnRenamed('avg(similarity)', 'similarity').withColumn('rank', func.dense_rank().over(Window.partitionBy('customer_id').orderBy(desc('similarity')))).filter(col('rank') <= 100)
+    c2r_df = c2r_df.groupBy("customer_id").agg(func.collect_list(func.struct('recommendation', 'similarity')).alias('recommendation_struct_list'))
+    customer_2_products = {row['customer_id']: row['products'] for row in c2p_df.groupBy(['customer_id']).agg(func.collect_list('product').alias('products')).collect()}
+    sort_udf = udf(lambda customer_id, l: [ele[0] for ele in sorted(l, key=lambda e: -e[1]) if ele[0] not in customer_2_products[customer_id]])
+    c2r_df = c2r_df.select("customer_id", sort_udf("customer_id", "recommendation_struct_list").alias("recommendations"))
+
+    rows = []
+    for row in c2r_df.collect():
+        rows.append((platform, row['customer_id'], 'user', 'bought', algo, str(row['recommendations'])))
+
+#    customer_ids = list(customer_2_products_purchased.keys())
+#    if limit:
+#        customer_ids = customer_ids[:limit]
+#
+#    customer_ids_chunks = [customer_ids[i:i+100] for i in range(0, len(customer_ids), 100)]
+#    Parallel(n_jobs=30, verbose=1, pre_dispatch='1.5*n_jobs', backend="threading")(delayed(compute_recommendation_rows)(customer_ids_chunk, 'user', 'bought', algo, customer_2_products_purchased, rows, direct_similar_products_dict) for customer_ids_chunk in customer_ids_chunks)
+
     print('Total number of customers: %d' % len(rows))
-    RecommendationsUtils.add_recommendations_in_mysql(Utils.mysqlConnection(env), 'recommendations_v2', rows)
-
-    #parent_2_children = defaultdict(lambda: [])
-    #for child, parent in results['child_2_parent'].items():
-        #parent_2_children[parent].append(child)
-
-    #print('Total Number of parent products: %d' % len(parent_2_children))
-    #print('Total Number of child products: %d' % sum([len(variants) for parent, variants in parent_2_children.items()]))
-    #rows = []
-    #product_ids_updated = []
-    #for product_id in direct_similar_products_dict:
-        #product_ids_updated.append(product_id)
-        #direct_similar_products = list(map(lambda e: int(e[0]), sorted(direct_similar_products_dict[product_id], key=lambda e: e[1], reverse=True)))
-        #rows.append((platform, product_id, 'product', 'bought', cab_algo, json.dumps(direct_similar_products)))
-        #variants = parent_2_children.get(product_id, [])
-        #for variant in variants:
-            #product_ids_updated.append(variant)
-            #rows.append((platform, variant, 'product', 'bought', cab_algo, str(direct_similar_products)))
-
-    #print('Adding recommendations for %d products in DB' % len(product_ids_updated))
-    #print('Total number of rows: %d' % len(rows))
-    #RecommendationsUtils.add_recommendations_in_mysql(Utils.mysqlConnection(env), 'recommendations_v2', rows)
+    RecommendationsUtils.add_recommendations_in_mysql(Utils.mlMysqlConnection(env), 'recommendations_v2', rows)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -384,7 +397,7 @@ if __name__ == '__main__':
     parser.add_argument('--p2p-start-datetime')
     parser.add_argument('--p2p-end-datetime')
     parser.add_argument('--customer-id')
-    parser.add_argument('--algo', default='combined_coccurence_direct')
+    parser.add_argument('--algo', default='average_coccurence_direct')
     parser.add_argument('--orders-count', type=int, default=10)
     parser.add_argument('--limit', type=int)
     parser.add_argument('--env', required=True)
