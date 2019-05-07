8109bbf9c52f1a39785197070e2c0409f7d9ccd1
Preprod Machine <gludonykaaa@gmail.com>
2019-05-07 12:14:49 +0000

    taking code from split_api

--- sqs_listener/cron_pas_listener_es.py
+++ sqs_listener/cron_pas_listener_es.py
@@ -4,7 +4,8 @@ from dateutil import tz
 #from pipelineUtils import PipelineUtils
 from datetime import datetime, timedelta
 
-sys.path.append('/home/apis/nykaa/')
+#sys.path.append('/home/apis/nykaa/')
+sys.path.append('/home/apis/nykaa_price_availabilty_api')
 from pas.v2.utils import Utils, MemcacheUtils, CATALOG_COLLECTION_ALIAS
 from nykaa.settings import *
 import argparse
