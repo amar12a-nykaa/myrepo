982cc195bd63ec495c25516f18538629c44649b0
Mayank Jaiswal <msjaiswal@gmail.com>
2019-05-30 02:21:04 +0000

    multithreaded cron consumer

--- sqs_listener/cron_pas_listener_es.py
+++ sqs_listener/cron_pas_listener_es.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
-import time
+import argparse
+import boto3
+import elasticsearch
+import json
+import queue
+import subprocess
 import sys
+import threading
+import time
+import traceback
 from dateutil import tz
 from datetime import datetime, timedelta
 
-sys.path.append('/var/www/pds_api/')
+sys.path.append("/var/www/pds_api/")
 from pas.v2.utils import Utils as PasUtils
+from nykaa.settings import DISCOVERY_SQS_ENDPOINT
+
 sys.path.append("/var/www/discovery_api")
 from disc.v2.utils import Utils as DiscUtils
 
-from nykaa.settings import DISCOVERY_SQS_ENDPOINT
 
-import argparse
-import elasticsearch
-import subprocess
-import queue
-import threading
-import traceback
-import json
+
 total = 0
 CHUNK_SIZE = 200
-NUMBER_OF_THREADS = 4
+
+
 def synchronized(func):
     func.__lock__ = threading.Lock()
 
@@ -30,6 +34,7 @@ def synchronized(func):
 
     return synced_func
 
+
 @synchronized
 def incrementGlobalCounter(increment):
     global total
@@ -37,154 +42,130 @@ def incrementGlobalCounter(increment):
     total = curr
     return total
 
-class Worker(threading.Thread):
-    def __init__(self, q):
+
+class WorkerThread(threading.Thread):
+    def __init__(self, q, name):
         self.q = q
-        super().__init__()
+        super().__init__(name=name)
 
     def run(self):
         while True:
             try:
-                product_chunk = self.q.get(timeout=3)  # 3s timeout
-                ScheduledPriceUpdater.updateChunkPrice(product_chunk)
+                chunk = self.q.get(timeout=3)  # 3s timeout
+                if chunk is None:
+                    print(self.name + " Dying now.")
+                    self.q.task_done()
+                    break
+                else:
+                    SQSConsumer.upload_one_chunk(chunk, threadname=self.name)
+                    self.q.task_done()
             except queue.Empty:
-                return
-            self.q.task_done()
+                print(self.name + " zz..")
+                time.sleep(15)
+                continue
 
 
 def getCurrentDateTime():
     current_datetime = datetime.utcnow()
-    from_zone = tz.gettz('UTC')
-    to_zone = tz.gettz('Asia/Kolkata')
+    from_zone = tz.gettz("UTC")
+    to_zone = tz.gettz("Asia/Kolkata")
     current_datetime = current_datetime.replace(tzinfo=from_zone)
     current_datetime = current_datetime.astimezone(to_zone)
     return current_datetime
 
+
 print("=" * 30 + " %s ======= " % getCurrentDateTime())
 
-def getCount():
-    return int(subprocess.check_output(
-        "ps aux | grep python | grep cron_pas_listener_es.py | grep -vE 'vim|grep|/bin/sh' | wc -l ",
-        shell=True).strip())
+
+ES_BULK_UPLOAD_BATCH_SIZE = 1000
+NUMBER_OF_THREADS = 2
 
 
-if getCount() >= 2:
-    #print("getCount(): %r" % getCount())
-    print("[%s] This script is already running. Exiting without doing anything" % getCurrentDateTime())
-    str(
-        subprocess.check_output("ps aux | grep python | grep cron_pas_listener_es.py | grep -vE 'vim|grep|/bin/sh' ",
-                                shell=True))
-    exit()
-
-
-class ScheduledPriceUpdater:
-
+class SQSConsumer:
     @classmethod
-    def getChunks(cls, data_list, chunk_size):
-        """Yield successive n-sized chunks from list l."""
-        for i in range(0, len(data_list), chunk_size):
-            yield data_list[i:i + chunk_size]
+    def __init__(self):
+        self.q = queue.Queue(maxsize=0)
+        self.sqs = boto3.client("sqs")
+        self.threads = []
 
-    def updateChunkPrice(product_chunk):
-        products = []
-        sku_list = []
-        psku_list = []
+        for i, _ in enumerate(range(NUMBER_OF_THREADS)):
+            name = "T" + str(i)
+            thread = WorkerThread(self.q, name=name)
+            thread.start()
+            self.threads.append(thread)
 
-        for single_product in product_chunk:
-            sku_list.append(single_product['sku'])
-            psku_list.append(single_product['psku'])
-            products.append({'sku': single_product['sku'], 'type': single_product['type']})
-            if single_product['psku'] and single_product['psku'] != single_product['sku']:
-                products.append({'sku': single_product['psku'], 'type': 'configurable'})
-
-        new_sku_list = list(set(sku_list) | set(psku_list))
-        sku_string = "','".join(new_sku_list)
-        query = "SELECT product_sku, bundle_sku FROM bundle_products_mappings WHERE product_sku in('" + sku_string + "')"
-        mysql_conn = PasUtils.mysqlConnection('r')
-        results = PasUtils.fetchResults(mysql_conn, query)
-        mysql_conn.close()
-        for res in results:
-            products.append({'sku': res['bundle_sku'], 'type': 'bundle'})
-
-        products = [dict(t) for t in {tuple(d.items()) for d in products}]
-        update_docs = []
-        try:
-            update_docs = PipelineUtils.getProductsToIndex(products, add_limit = True)
-            if update_docs:
-                DiscUtils.updateESCatalog(update_docs)
-        except Exception as e:
-            print(traceback.format_exc())
-
-        total_count = incrementGlobalCounter(len(update_docs))
-        print("[%s] Update progress: %s products updated" % (getCurrentDateTime(), total_count))
-
-
-    def update():
-        q = queue.Queue(maxsize=0)
-        current_datetime = getCurrentDateTime()
-        last_datetime = current_datetime - timedelta(hours=1)
-        from IPython import embed
-        import boto3
-        # Create SQS client
-        sqs = boto3.client('sqs')
-        queue_url = DISCOVERY_SQS_ENDPOINT
+    def start(self,):
         # Receive message from SQS queue
         startts = time.time()
         update_docs = []
-        is_queue_empty=False
-        while True:
-          response = sqs.receive_message(
-              QueueUrl=queue_url,
-              AttributeNames=[
-                  'SentTimestamp'
-              ],
-              MaxNumberOfMessages=10,
-              MessageAttributeNames=[
-                  'All'
-              ],
-              VisibilityTimeout=30,
-              WaitTimeSeconds=0
-          )
-          if 'Messages' in response:
-            for message in response['Messages']:
-              #message = response['Messages'][0]
-              receipt_handle = message['ReceiptHandle']
-              update_docs.append(json.loads(message['Body'])[0])
-              sqs.delete_message(
-                QueueUrl=queue_url,
-                ReceiptHandle=receipt_handle
-              ) 
-          else:
-            is_queue_empty=True
-            print("No more messages")
+        is_sqs_empty = False
+        num_consecutive_empty_checks = 0
+        num_products_processed = 0
 
-          if len(update_docs)==1000 or (len(update_docs)>=1 and is_queue_empty) :
-            try:
-              print("Sending %s docs to bulk upload" % len(update_docs))
-              DiscUtils.updateESCatalog(update_docs)
-              print("Done with one batch of bulk upload")
-            except elasticsearch.helpers.BulkIndexError as e:
-              missing_skus = []
-              for error in e.errors:
-                if(error['update']['error']['type'] == "document_missing_exception"):
-                  missing_skus.append(error['update']['data']['doc']['sku'])
+        while not is_sqs_empty:
+            response = self.sqs.receive_message(
+                QueueUrl=DISCOVERY_SQS_ENDPOINT,
+                AttributeNames=["SentTimestamp"],
+                MaxNumberOfMessages=10,
+                MessageAttributeNames=["All"],
+                VisibilityTimeout=30,
+                WaitTimeSeconds=0,
+            )
+            from IPython import embed
+
+            if "Messages" in response:
+                for message in response["Messages"]:
+                    receipt_handle = message["ReceiptHandle"]
+                    update_docs += json.loads(message["Body"])
+                    self.sqs.delete_message(QueueUrl=DISCOVERY_SQS_ENDPOINT, ReceiptHandle=receipt_handle)
+            else:
+                is_sqs_empty = True
+                print("SQS is empty!")
+
+            if len(update_docs) >= ES_BULK_UPLOAD_BATCH_SIZE or (len(update_docs) >= 1 and is_sqs_empty):
+                print("Main Thread: Putting chunk of size %s in queue " % len(update_docs))
+                self.q.put_nowait(update_docs)
+                num_products_processed += len(update_docs)
+                update_docs = []
+
+        # stop workers
+        for _ in range(NUMBER_OF_THREADS):
+            print("Adding None to queue")
+            self.q.put(None)
+
+        print("Waiting for threads to finish .. ")
+        for thread in self.threads:
+            thread.join()
+
+        print("All threads finished")
+        time_taken = time.time() - startts
+        speed = round(num_products_processed / time_taken * 1.0)
+        print("Number of products processed: %s @ %s products/sec" % (num_products_processed, speed))
+
+    @classmethod
+    def upload_one_chunk(cls, chunk, threadname):
+        try:
+            update_docs = chunk
+            print(threadname + ": Sending %s docs to bulk upload" % len(update_docs))
+            DiscUtils.updateESCatalog(update_docs, refresh=True)
+            print(threadname + ": Done with one batch of bulk upload")
+        except elasticsearch.helpers.BulkIndexError as e:
+            missing_skus = []
+            for error in e.errors:
+                if error["update"]["error"]["type"] == "document_missing_exception":
+                    missing_skus.append(error["update"]["data"]["doc"]["sku"])
                 else:
-                  print("Unhandled Error for sku '%s' - %s" % (error['update']['data']['doc']['sku'], error['update']['error']['type'])  )
-              if missing_skus:
-                print("Missing SKUs in ES %s" % missing_skus)  
-            finally:
-              update_docs.clear()
-          if is_queue_empty:
-            break
+                    print(
+                        "Unhandled Error for sku '%s' - %s"
+                        % (error["update"]["data"]["doc"]["sku"], error["update"]["error"]["type"])
+                    )
+            print("Missing Skus", len(missing_skus))
 
-        endts=time.time()
-        diff = endts - startts
-        print("Time taken:  %s seconds" % diff)
-        exit()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", type=int, help='number of records in single index request')
-    parser.add_argument("--threads", type=int, help='number of records in single index request')
+    parser.add_argument("--batch_size", type=int, help="number of records in single index request")
+    parser.add_argument("--threads", type=int, help="number of records in single index request")
     argv = vars(parser.parse_args())
-    ScheduledPriceUpdater.update()
+    consumer = SQSConsumer()
+    consumer.start()
