d1e37c22b782a10bb110d10d1b851482fac1e95e
niharika <niharika.vallambatta@nykaa.com>
2019-06-28 15:46:53 +0530

    popularity calculation changes
    Signed-off-by: niharika <niharika.vallambatta@nykaa.com>

--- autocomplete/normalize_searches_daily.py
+++ autocomplete/normalize_searches_daily.py
@@ -153,7 +153,8 @@ def normalize_search_terms():
         
         print("Computing popularity")
         df = pd.DataFrame(bucket_results)
-        df['ctr'] = (df['count'])*100.0/df['click_interaction_instance']
+        df['click_interaction_instance'] = df['click_interaction_instance'].fillna(0)
+        df['ctr'] = (df['click_interaction_instance'])*100.0/df['count']
         df['norm_count'] = normalize(df['count'])
         multiplication_factor = POPULARITY_DECAY_FACTOR ** (bucket_id + 1)
         df['popularity'] = df['ctr']*df['click_interaction_instance']
