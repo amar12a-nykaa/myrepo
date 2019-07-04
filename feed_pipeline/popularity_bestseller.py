54dc3190de4fb400228914aa2758e40a04381a37
Shweta <shweta.agrawal@nykaa.com>
2019-07-04 19:00:44 +0530

    pass bestseller tag to parent.
    Signed-off-by: Shweta <shweta.agrawal@nykaa.com>

--- feed_pipeline/popularity_bestseller.py
+++ feed_pipeline/popularity_bestseller.py
@@ -29,18 +29,19 @@ def get_order_data(startdate, enddate):
   bucket_results = []
   for p in order_table.aggregate([
     {"$match": {"date": {"$gte": startdate, "$lte": enddate}}},
-    {"$group": {"_id": {"product_id": "$product_id"},
+    {"$group": {"_id": {"product_id": "$product_id", "parent_id": "$parent_id"},
                 "orders": {"$sum": "$orders"}
                 }
     }
   ]):
     p['product_id'] = p['_id']['product_id']
+    p['parent_id'] = p['_id']['parent_id']
     p.pop("_id")
     bucket_results.append(p)
   
   data = pd.DataFrame(bucket_results)
   print(data.columns)
-  data = data.astype({'product_id': str})
+  data = data.astype({'product_id': str, 'parent_id': str})
   return data
 
 
@@ -61,6 +62,8 @@ def calculate_bestseller_category(order_data):
     row = dict(row)
     try:
       bestseller_data.update({"_id": row['product_id']}, {"type": "category", "key": row['l3_id']}, upsert=True)
+      if row['parent_id'] != row['product_id']:
+        bestseller_data.update({"_id": row['parent_id']}, {"type": "category", "key": row['l3_id']}, upsert=True)
     except:
       print("[ERROR] bestseller_data.update error %s " %row['product_id'])
       raise
@@ -90,6 +93,8 @@ def calculate_bestseller_brand(order_data):
     row = dict(row)
     try:
       bestseller_data.update({"_id": row['product_id']}, {"type": "brand", "key": row['brand_code']}, upsert=True)
+      if row['parent_id'] != row['product_id']:
+        bestseller_data.update({"_id": row['parent_id']}, {"type": "category", "key": row['l3_id']}, upsert=True)
     except:
       print("[ERROR] bestseller_data.update error %s " % row['product_id'])
       raise
@@ -102,7 +107,7 @@ if __name__ == '__main__':
   enddate = arrow.now().replace(days=0, hour=0, minute=0, second=0, microsecond=0,
                                 tzinfo=None).datetime.replace(tzinfo=None)
   order_data = get_order_data(startdate, enddate)
-  order_data = pd.merge(order_data, product_validity, how='left', on=['product_id'])
+  order_data = pd.merge(order_data, product_validity, how='left', on=['product_id', 'parent_id'])
   order_data.valid = order_data.valid.fillna(1)
   order_data.orders = order_data.apply(lambda x: 0 if not x['valid'] else x['orders'], axis=1)
 
