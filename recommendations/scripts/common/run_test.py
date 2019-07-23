5ba93d98f47ad6fe306ea188d4b8c5d9adb183c2
ashwin <ashwin010293@gmail.com>
2019-07-23 17:18:22 +0000

    adding test emt

---
+++ recommendations/scripts/common/run_test.py
@@ -0,0 +1,16 @@
+import boto3
+import json
+import traceback
+import sys
+sys.path.append('/home/ashwinpal/nykaa_scripts/utils')
+import constants as Constants
+from s3utils import S3Utils
+from recoutils import RecoUtils
+from emrutils import EMRUtils
+
+
+if __name__ == '__main__':
+    command_line_arguments = sys.argv[1:]
+    config = '%s/%s' % (Constants.HOME_DIR, Constants.SMALL_EMR_CONFIG)
+    env_details = RecoUtils.get_env_details()
+    EMRUtils.launch_spark_emr('TEST', config, [], [], Constants.SMALL_INSTANCE)
