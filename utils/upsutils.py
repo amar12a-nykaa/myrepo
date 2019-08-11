62cd8a23c9b323f1d936606626d6bcf124251d2f
Ubuntu <ashwin010293@gmail.com>
2019-08-11 12:05:59 +0000

    gen_top_categories now using upsutils for pushing categories recommendations into ups

--- utils/upsutils.py
+++ utils/upsutils.py
@@ -27,7 +27,7 @@ env_details = RecoUtils.get_env_details()
 
 class UPSUtils:
 
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')
 
     def get_table_name():
         if env_details['env'] == 'prod':
@@ -35,22 +35,24 @@ class UPSUtils:
         else:
             return 'user_profile_service_mumbai'
 
-    def _add_recommendations_in_ups(rows):
+    def _add_recommendations_in_ups(field_name, rows):
         keys = [{'user_id': '%s' % customer_id} for customer_id in rows.keys()]
         table_name = UPSUtils.get_table_name()
         get_response = UPSUtils.dynamodb.batch_get_item(RequestItems={table_name: {'Keys': keys}})
         put_items = []
         for user_obj in get_response['Responses'][table_name]:
-            if user_obj.get('recommendations'):
-                user_obj['recommendations'].update(rows[user_obj['user_id']])
+            if user_obj.get(field_name):
+                user_obj[field_name].update(rows[user_obj['user_id']])
             else:
-                user_obj['recommendations'] = rows[user_obj['user_id']]
+                user_obj[field_name] = rows[user_obj['user_id']]
             put_items.append({'PutRequest': {'Item': user_obj}})
         for i in range(0, len(put_items), 25):
             UPSUtils.dynamodb.batch_write_item(RequestItems={table_name: put_items[i:i+25]})
 
-    def add_recommendations_in_ups(rows):
+    def add_recommendations_in_ups(field_name, rows):
+        table_name = UPSUtils.get_table_name()
+        print(UPSUtils.dynamodb.Table(table_name).creation_date_time)
         rows_chunks = [rows[i:i+100] for i in range(0, len(rows), 100)]
         for chunk in rows_chunks:
-            UPSUtils._add_recommendations_in_ups({str(row['customer_id']): row['value'] for row in chunk})
+            UPSUtils._add_recommendations_in_ups(field_name, {str(row['customer_id']): row['value'] for row in chunk})
 
