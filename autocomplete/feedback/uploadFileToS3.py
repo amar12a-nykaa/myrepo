cf07e7c55710c2a9e9211a984510d772c6f073b1
Shweta <shweta.agrawal@nykaa.com>
2019-08-13 16:32:32 +0530

    rename file in s3 as well.
    Signed-off-by: Shweta <shweta.agrawal@nykaa.com>

--- autocomplete/feedback/uploadFileToS3.py
+++ autocomplete/feedback/uploadFileToS3.py
@@ -1,9 +1,8 @@
 import boto3
-import os
 import sys
 import argparse
-import datetime
-import zipfile
+import re
+import time
 
 sys.path.append("/nykaa/scripts/sharedutils")
 from dateutils import enumerate_dates
@@ -17,39 +16,87 @@ days = -1 * argv['days']
 
 dates_to_process = enumerate_dates(days, -1)
 pipeline = boto3.session.Session(profile_name='datapipeline')
-s3 = pipeline.client('s3')
 bucket_name = PipelineUtils.getBucketNameForFeedback()
 
-def unzip_file(path_to_zip_file):
-    zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
-    zip_ref.extractall(os.path.dirname(path_to_zip_file))
-    zip_ref.close()
+params = {
+    'region': 'ap-southeast-1',
+    'database': 'datapipeline',
+    'bucket': bucket_name
+}
+client = pipeline.client('athena', region_name=params["region"])
 
-def uploadToS3(filepath, filename):
-    s3.upload_file(filepath, bucket_name, filename)
-    print('file uploaded successfully')
+def query_athena(params):
+    response = client.start_query_execution(
+        QueryString=params["query"],
+        QueryExecutionContext={
+            'Database': params['database']
+        },
+        ResultConfiguration={
+            'OutputLocation': 's3://' + params['bucket'] + '/' + params['path']
+        }
+    )
+    return response
 
 for date in dates_to_process:
-    print("=== READING CSV FOR : %s ====" % date)
-    filename = 'autocompleteFeedback%s.csv' % date.strftime("%Y%m%d")
-    filepath = '/nykaa/adminftp/autocompleteFeedback%s.csv' % date.strftime("%Y%m%d")
-    if not os.path.exists(filepath):
-        filename_zip = '/nykaa/adminftp/autocompleteFeedback%s.zip' % date.strftime("%Y%m%d")
-        if not os.path.isfile(filename_zip):
-            print("%s not found" %filename_zip)
-            continue
-        unzip_file(filename_zip)
-    os.system('sed -i "s/, 201/ 201/g" %s' % filepath)
-    os.system('sed -i "s/\\"//g" %s' % filepath)
-    os.system('sed -i "s/,Typed Search Term (evar77),/,typed_term,/g" %s' % filepath)
-    os.system('sed -i "s/,Internal Search Term (Conversion) (evar6),/,search_term,/g" %s' % filepath)
-    os.system('sed -i "s/,Internal Search Term (Conversion) Instance (Instance of evar6)/,click_count/g" %s' % filepath)
+    s3_file_location = 'dt=%s' % date.strftime("%Y%m%d")
+    dateStr = date.strftime("%Y-%m-%d")
+    query = """SELECT b.*,
+                      a.click_freq
+                FROM
+                    (SELECT DISTINCT lower(typed_term) AS typed_term,
+                         lower(clicked_term) AS clicked_term,
+                         count(*) AS click_freq
+                    FROM _events
+                    WHERE visible_sugg!=''
+                            AND CAST(dt AS DATE) = DATE('%s')
+                    GROUP BY  1,2 ) a
+                RIGHT JOIN
+                    (SELECT DISTINCT lower(typed_term) AS typed_term,
+                         lower(visible_term) AS visible_term,
+                         count(*) AS visible_freq
+                    FROM _events
+                    CROSS JOIN UNNEST(SPLIT(visible_sugg,'|')) AS t (visible_term)
+                    WHERE visible_sugg!=''
+                            AND CAST(dt AS DATE) = DATE('%s')
+                    GROUP BY  1,2 ) b
+                    ON a.typed_term=b.typed_term
+                        AND a.clicked_term=b.visible_term
+                ORDER BY  4 desc, 3 desc
+    """%(dateStr, dateStr)
+    params['path'] = s3_file_location
+    params['query'] = query
 
-    s3_file_location = 'dt=%s/autocompleteFeedback.csv' % date.strftime("%Y%m%d")
-    uploadToS3(filepath, s3_file_location)
+    execution = query_athena(params)
+    execution_id = execution['QueryExecutionId']
+    state = 'RUNNING'
 
-
-
-
-
-
+    max_execution = 20
+    terminated = False
+    outputFile = ""
+    while (max_execution > 0 and state in ['RUNNING']):
+        max_execution = max_execution - 1
+        response = client.get_query_execution(QueryExecutionId=execution_id)
+    
+        if 'QueryExecution' in response and \
+            'Status' in response['QueryExecution'] and \
+            'State' in response['QueryExecution']['Status']:
+            state = response['QueryExecution']['Status']['State']
+            if state == 'FAILED':
+                print("query execution failed")
+                terminated = True
+                break
+            elif state == 'SUCCEEDED':
+                s3_path = response['QueryExecution']['ResultConfiguration']['OutputLocation']
+                outputFile = re.findall('.*\/(.*)', s3_path)[0]
+                print("query execution successfull. File %s created"%outputFile)
+                terminated = True
+                break
+        time.sleep(6)
+    if outputFile:
+        s3 = pipeline.client('s3')
+        source = s3_file_location + '/' + outputFile
+        destination = s3_file_location + '/autocompleteFeedbackV2.csv'
+        sourcePath = bucket_name + "/" + source
+        s3.copy_object(Bucket=bucket_name, CopySource=sourcePath, Key=destination)
+        s3.delete_object(Bucket=bucket_name, Key=source)
+        print('file uploaded successfully')
