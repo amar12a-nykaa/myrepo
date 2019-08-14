3d46d98d799930fcbc0fb43728e6fc6c1aa68b64
niharika <niharika.vallambatta@nykaa.com>
2019-08-14 11:25:59 +0530

    fetching product_enable_time from doc
    Signed-off-by: niharika <niharika.vallambatla@nykaa.com>

--- feed_pipeline/catalog_indexer.py
+++ feed_pipeline/catalog_indexer.py
@@ -981,10 +981,10 @@ class CatalogIndexer:
                 doc['custom_tags'] = []
                 if doc['product_id'] in bestsellers:
                     doc['custom_tags'].append('BESTSELLER')
-                golive_time = row.get('product_enable_time', 0)
+                golive_time = doc.get('product_enable_time', 0)
                 today = dt.datetime.combine(dt.datetime.today(), dt.time.min)
                 startdate = today - dt.timedelta(days=30)
-                if golive_time and str(golive_time) >= str(startdate):
+                if golive_time and (str(golive_time) >= str(startdate)):
                     doc['custom_tags'].append('NEW')
                 
                 if search_engine == 'elasticsearch':
