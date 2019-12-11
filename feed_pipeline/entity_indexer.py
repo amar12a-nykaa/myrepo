b3f21a57cf0f7e745e8713a1f6d315fadc7a0c21
Shweta <shweta.agrawal@nykaa.com>
2019-12-11 17:39:04 +0530

    dict fixes.
    Signed-off-by: Shweta <shweta.agrawal@nykaa.com>

--- feed_pipeline/entity_indexer.py
+++ feed_pipeline/entity_indexer.py
@@ -33,9 +33,9 @@ sys.path.append("/var/www/discovery_api")
 from disc.v2.utils import Utils as DiscUtils
 
 filter_attribute_map = [("656","concern"), ("661","preference"), ("659","formulation"), ("664","finish"), ("658","color"),
-                        ("gender", "655"), ("skin_type", "657"), ("hair_type", "677"), ("ingredient", "857"),
-                        ("skin_tone", "665"), ("coverage", "663"), ("wiring", "812"), ("padding", "813"),
-                        ("fabric", "815"), ("pattern", "822"), ("rise", "823")]
+                        ("655","gender"), ("657","skin_type"), ("677","hair_type"), ("857","ingredient"),
+                        ("665","skin_tone"), ("663","coverage"), ("812","wiring"), ("813","padding"),
+                        ("815","fabric"), ("822","pattern"), ("823","rise")]
 FILTER_WEIGHT = 50
 ASSORTMENT_WEIGHT = 1
 
@@ -186,42 +186,40 @@ class EntityIndexer:
     EsUtils.indexDocs(docs, collection)
 
   def index_filters(collection):
-    mysql_conn = PasUtils.nykaaMysqlConnection(force_production=True)
-    synonyms = {
-      {'10777': {'name': 'Acne/Blemishes', 'synonym': ['acne', 'anti acne', 'blemishes', 'anti blemishes']}},
-      {'10753': {'name': 'Dark Spots/Pigmentation', 'synonym': ['dark spots', 'pigmentation', 'anti pigmentation']}},
-      {'10771': {'name': 'Pores/Blackheads/Whiteheads', 'synonym': ['pores', 'blackheads', 'whiteheads']}},
-      {'10749': {'name': 'Fine Lines/Wrinkles', 'synonym': ['fines lines', 'wrinkle', 'anti wrinkle']}},
-      {'10776': {'name': 'Anti-ageing', 'synonym': ['ageing', 'anti ageing']}},
-      {'10770': {'name': 'Brightening/Fairness', 'synonym': ['brightening', 'fairness']}},
-      {'91637': {'name': 'Hairfall & Thinning', 'synonym': ['anti hairfall', 'hairfall', 'thinning']}},
-      {'91638': {'name': 'Dry & Frizzy Hair', 'synonym': ['dry hair', 'frizzy hair']}},
-      {'10755': {'name': 'Dandruff', 'synonym': ['dandruff', 'anti dandruff']}},
-      {'80231': {'name': 'Tan Removal', 'synonym': ['tan removal', 'tan', 'anti tan', 'de tan']}},
-      {'12089': {'name': 'Lotion/Body Butter', 'synonym': ['lotion', 'body butter']}},
-      {'10711': {'name': 'Female', 'synonym': ['women', 'woman', 'ladies']}},
-      {'11075': {'name': 'Normal', 'synonym': ['normal hair']}},
-      {'11079': {'name': 'Curly', 'synonym': ['curly hair']}},
-      {'11073': {'name': 'Straight', 'synonym': ['straight hair']}},
-      {'11076': {'name': 'Fine', 'synonym': ['fine hair']}},
-      {'11074': {'name': 'Oily', 'synonym': ['oily hair']}},
-      {'11078': {'name': 'Dryness', 'synonym': ['dry hair']}},
-      {'11077': {'name': 'Dull Hair', 'synonym': ['dull hair']}},
-      {'11072': {'name': 'Thick', 'synonym': ['thick hair']}},
-      {'11071': {'name': 'Thin', 'synonym': ['thin hair']}},
-      {'91639': {'name': 'Wavy', 'synonym': ['wavy hair']}},
-      {'91643': {'name': 'Argan Oil', 'synonym': ['argan']}},
-      {'67293': {'name': 'Solid/Plain', 'synonym': ['solid', 'plain']}},
-      {'96358': {'name': 'Embellished/Sequined', 'synonym': ['embellished', 'sequined']}},
-      {'10887': {'name': 'Medium/Wheatish', 'synonym': ['medium skin', 'wheatish skin']}},
-      {'10886': {'name': 'Fair/Light', 'synonym': ['fair skin', 'light skin']}},
-      {'10888': {'name': 'Dusky/Dark', 'synonym': ['dusky skin', 'dark skin']}},
-      {'10781': {'name': 'Dry', 'synonym': ['dry skin']}},
-      {'10779': {'name': 'Oily', 'synonym': ['oily skin']}},
-      {'10780': {'name': 'Normal', 'synonym': ['normal skin']}},
-      {'10778': {'name': 'Sensitive', 'synonym': ['sensitive skin']}},
-      {'10782': {'name': 'Combination', 'synonym': ['combination skin']}}
-    }
+    mysql_conn = PasUtils.nykaaMysqlConnection()
+    synonyms = {'10777': {'name': 'Acne/Blemishes', 'synonym': ['acne', 'anti acne', 'blemishes', 'anti blemishes']},
+                '10753': {'name': 'Dark Spots/Pigmentation', 'synonym': ['dark spots', 'pigmentation', 'anti pigmentation']},
+                '10771': {'name': 'Pores/Blackheads/Whiteheads', 'synonym': ['pores', 'blackheads', 'whiteheads']},
+                '10749': {'name': 'Fine Lines/Wrinkles', 'synonym': ['fines lines', 'wrinkle', 'anti wrinkle']},
+                '10776': {'name': 'Anti-ageing', 'synonym': ['ageing', 'anti ageing']},
+                '10770': {'name': 'Brightening/Fairness', 'synonym': ['brightening', 'fairness']},
+                '91637': {'name': 'Hairfall & Thinning', 'synonym': ['anti hairfall', 'hairfall', 'thinning']},
+                '91638': {'name': 'Dry & Frizzy Hair', 'synonym': ['dry hair', 'frizzy hair']},
+                '10755': {'name': 'Dandruff', 'synonym': ['dandruff', 'anti dandruff']},
+                '80231': {'name': 'Tan Removal', 'synonym': ['tan removal', 'tan', 'anti tan', 'de tan']},
+                '12089': {'name': 'Lotion/Body Butter', 'synonym': ['lotion', 'body butter']},
+                '10711': {'name': 'Female', 'synonym': ['women', 'woman', 'ladies']},
+                '11075': {'name': 'Normal', 'synonym': ['normal hair']},
+                '11079': {'name': 'Curly', 'synonym': ['curly hair']},
+                '11073': {'name': 'Straight', 'synonym': ['straight hair']},
+                '11076': {'name': 'Fine', 'synonym': ['fine hair']},
+                '11074': {'name': 'Oily', 'synonym': ['oily hair']},
+                '11078': {'name': 'Dryness', 'synonym': ['dry hair']},
+                '11077': {'name': 'Dull Hair', 'synonym': ['dull hair']},
+                '11072': {'name': 'Thick', 'synonym': ['thick hair']},
+                '11071': {'name': 'Thin', 'synonym': ['thin hair']},
+                '91639': {'name': 'Wavy', 'synonym': ['wavy hair']},
+                '91643': {'name': 'Argan Oil', 'synonym': ['argan']},
+                '67293': {'name': 'Solid/Plain', 'synonym': ['solid', 'plain']},
+                '96358': {'name': 'Embellished/Sequined', 'synonym': ['embellished', 'sequined']},
+                '10887': {'name': 'Medium/Wheatish', 'synonym': ['medum skin', 'wheatish skin']},
+                '10886': {'name': 'Fair/Light', 'synonym': ['fair skin', 'light skin']},
+                '10888': {'name': 'Dusky/Dark', 'synonym': ['dusky skin', 'dark skin']},
+                '10781': {'name': 'Dry', 'synonym': ['dry skin']},
+                '10779': {'name': 'Oily', 'synonym': ['oily skin']},
+                '10780': {'name': 'Normal', 'synonym': ['normal skin']},
+                '10778': {'name': 'Sensitive', 'synonym': ['sensitive skin']},
+                '10782': {'name': 'Combination', 'synonym': ['combination skin']}}
     for filt in filter_attribute_map:
       id = filt[0]
       filter = filt[1]
@@ -243,7 +241,7 @@ class EntityIndexer:
           "id": str(row['filter_id'])
         }
         if filter_doc["id"] in synonyms:
-          filter_doc["entity_synonyms"] = synonyms[filter_doc["id"]]
+          filter_doc["entity_synonyms"] = synonyms[filter_doc["id"]]['synonym']
         docs.append(filter_doc)
         if len(docs) >= 100:
           EsUtils.indexDocs(docs, collection)
