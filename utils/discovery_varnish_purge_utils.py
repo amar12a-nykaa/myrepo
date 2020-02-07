64ace7aebd7b45865b36844b66c28e2614d137cf
goyalsaurav <sgoyal.jjr@gmail.com>
2020-02-07 11:45:49 +0530

    added error logs updated

--- utils/discovery_varnish_purge_utils.py
+++ utils/discovery_varnish_purge_utils.py
@@ -2,16 +2,43 @@ import sys
 import boto3
 import json
 import traceback
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 sys.path.append("/home/ubuntu/nykaa_scripts/")
 from feed_pipeline.pipelineUtils import PipelineUtils
 
 CHUNK_SIZE = 20
+filename = "/var/log/discovery_purge_sqs/discovery_purge_sqs.log"
+def create_log_file():
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
 
 def chunkify(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
+def log_info(purge_doc,status,exception):
+    logger = logging.getLogger()
+    create_log_file()
+    file_logger = RotatingFileHandler(filename, maxBytes=50000, backupCount=5)
+    SIMPLE_LOG_FORMAT = {
+        "status": "%(status)s",
+        "msg": "%(message)s",
+        "ts": "%(asctime)s",
+        "sku_ids": "%(sku_ids)s",
+        "source": "%(source)s",
+    }
+    extra = {'source': purge_doc['source'],'sku_ids': purge_doc['sku_ids'], 'status': status}
+    if exception:
+        SIMPLE_LOG_FORMAT["exception"] = "%(exception)s"
+        extra['exception'] = type(exception).__name__
+    file_logger.setFormatter(logging.Formatter(json.dumps(SIMPLE_LOG_FORMAT)))
+    file_logger.setLevel(logging.ERROR)
+    file_logger.setLevel(logging.INFO)
+    logger.addHandler(file_logger)
+    logger.info("Discovery Varnish Purge Sqs insertion", extra=extra)
 
 def insert_in_varnish_purging_sqs(docs,source):
     sku_list = [doc.get('sku',"") for doc in docs]
@@ -30,9 +57,8 @@ def insert_in_varnish_purging_sqs(docs,source):
                 MessageAttributes={},
                 MessageBody=(json.dumps(purge_doc, default=str))
             )
-        except:
+            log_info(purge_doc, "success")
+        except Exception as e:
             print(traceback.format_exc())
             print("Insertion in SQS failed for skus %s and source %s" % (purge_doc['sku_ids'], purge_doc['source']))
-            with open('/var/log/discovery_purge_sqs_error.log','a') as the_file:
-                the_file.write(traceback.format_exc())
-                the_file.write("Insertion in SQS failed for skus %s and source %s" % (purge_doc['sku_ids'], purge_doc['source']))
+            log_info(purge_doc, "failure", e)
