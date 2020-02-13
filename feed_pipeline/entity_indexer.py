ef6d363b66876fb10eb4f4171fa529d0248fb2a6
Shweta2017 <shweta.agrawal@nykaa.com>
2020-02-13 13:56:09 +0530

    primary category changes.
    Signed-off-by: Shweta2017 <shweta.agrawal@nykaa.com>

--- feed_pipeline/entity_indexer.py
+++ feed_pipeline/entity_indexer.py
@@ -126,7 +126,11 @@ class EntityIndexer:
     df = df_cat.append([df_l1_cat])
     
     #TODO adding priority to category from external source
-    
+    category_priority = df.read_csv('primary_cateogry.csv')
+    category_priority["factor"] = 100
+    df = pd.merge(df, category_priority, how="left", on="category_id")
+    df.factor = df.factor.fillna(1)
+    df.weight = df.apply(lambda x: x['weight']*x['factor'], axis=1)
     df = df.sort_values(by='weight', ascending=False)
     primary_cat = df.drop_duplicates(subset=['store', 'name_id'], keep='first', inplace=False)
     df.id = df.id.astype(str)
