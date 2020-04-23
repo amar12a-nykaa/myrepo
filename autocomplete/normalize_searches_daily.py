38dbd38fc5db9ef9beead6ae90f888906a769f14
Shweta2017 <shweta.agrawal@nykaa.com>
2020-04-23 15:10:43 +0530

    added file support in corrections.
    Signed-off-by: Shweta2017 <shweta.agrawal@nykaa.com>

--- autocomplete/normalize_searches_daily.py
+++ autocomplete/normalize_searches_daily.py
@@ -73,6 +73,11 @@ create_missing_indices()
 def get_corrections_map():
     url = "https://nyk-aggregator-api.nykaa.com/api/getRedisData?key_type=query_replace&nested=true"
     response = json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
+
+    df = pd.read_csv("correction_file.csv")
+    for i, row in df.iterrows():
+        row = dict(row)
+        response[row["query"]] = row["corrected_query"]
     return response
 
 CORRECTIONS_MAP = get_corrections_map()
