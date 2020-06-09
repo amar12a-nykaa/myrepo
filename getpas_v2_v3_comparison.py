b90e869f84c093578d366562ee28091f9dd83471
goyalsaurav <sgoyal.jjr@gmail.com>
2020-06-09 12:02:28 +0530

    added completion progress and mail recipients

--- getpas_v2_v3_comparison.py
+++ getpas_v2_v3_comparison.py
@@ -11,18 +11,19 @@ from utils.mailutils import Mail
 
 
 CHUNK_SIZE = 100
-diff_csv_headers = ["sku", "quantity_v2", "quantity_v3", "backorders_v2", "backorders_v3", "msp_v2", "msp_v3", "expdt_v2",
-           "expdt_v3", "mrp_v2", "mrp_v3", "is_in_stock_v2", "is_in_stock_v3", "sp_v2", "sp_v3"]
+diff_csv_headers = ["sku", "type", "backorders_v2", "backorders_v3", "msp_v2", "msp_v3", "expdt_v2",
+           "expdt_v3", "mrp_v2", "mrp_v3", "is_in_stock_v2", "is_in_stock_v3", "sp_v2", "sp_v3", "jit_eretail_v2", "jit_eretail_v3"]
 diff_csv_filename = "diff_v2_v3.csv"
-MAIL_RECEIPIENTS = "discovery-tech@nykaa.com"
+fields_to_check = ["backorders", "msp", "expdt", "mrp", "is_in_stock", "sp", "jit_eretail"]
+MAIL_RECIPIENTS = "gaurav.sharma@nykaa.com, charu.sharma@nykaa.com, sandeep@euler-systems.com, kangkan@gludo.com, raman@gludo.com, kedar.pagdhare@nykaa.com, rishi.kataria@nykaa.com, saurav.goyal@nykaa.com"
 diff_exist = False
 number_of_rows = 0
 
 pas_url_v2 = "http://preprod-priceapi.nyk00-int.network/apis/v2/pas.get"
 pas_url_v3 = "http://preprod-priceapi.nyk00-int.network/apis/v3/pas.get"
 if socket.gethostname().startswith('admin'):
-    pas_url_v2 = "http://prod-priceapi.nyk00-int.network/apis/v2/pas.get"
-    pas_url_v3 = "http://prod-priceapi.nyk00-int.network/apis/v3/pas.get"
+    pas_url_v2 = "http://prod-api.nyk00-int.network/apis/v2/pas.get"
+    pas_url_v3 = "http://prod-api.nyk00-int.network/apis/v3/pas.get"
 
 
 def chunkify(l, n):
@@ -34,18 +35,17 @@ def compare_results(dict1={}, dict2={}):
     diff_list = []
     for key1, value1 in dict1.items():
         diff_dict = {}
+        diff_dict["sku"] = key1
+        diff_dict["type"] = value1.get("type")
         value2 = dict2.get(key1, {})
+        diff_added = False
 
-        # ignore from_inventory_service flag for result from v3
-        if value2:
-            value2.pop("from_inventory_service", None)
-
-        dict1_dict2 = {k+'_v2': v for k, v in value1.items() if value1[k] != value2.get(k)}
-        dict2_dict1 = {k+'_v3': v for k, v in value2.items() if value2[k] != value1.get(k)}
-        if dict1_dict2 or dict2_dict1:
-            diff_dict["sku"] = key1
-            diff_dict.update(dict1_dict2)
-            diff_dict.update(dict2_dict1)
+        for field in fields_to_check:
+            if value1.get(field) != value2.get(field):
+                diff_added = True
+                diff_dict[field + '_v2'] = value1.get(field)
+                diff_dict[field + '_v3'] = value2.get(field)
+        if diff_added:
             diff_list.append(diff_dict)
     return diff_list
 
@@ -65,13 +65,14 @@ def write_to_csv_file(diff_list=[]):
             print("I/O error")
 
 mysql_conn = PasUtils.mysqlConnection("r")
-query = "SELECT sku,type from products"
+query = "SELECT sku,type from products where disabled = 0"
 results = PasUtils.fetchResults(mysql_conn, query)
 
 chunks = chunkify(results, CHUNK_SIZE)
 
 write_headers_to_csv_file()
 
+chunks_completed = 0
 for chunk in chunks:
     request_data = {}
     request_data["products"] = chunk
@@ -97,6 +98,8 @@ for chunk in chunks:
     #insert into csv file
     if diff_list:
         write_to_csv_file(diff_list)
+    chunks_completed = chunks_completed + 1
+    print("CHUNKS_COMPLETED %s" % chunks_completed)
 
 with open(diff_csv_filename, 'r') as csvfile:
     reader = csv.reader(csvfile)
@@ -106,9 +109,9 @@ with open(diff_csv_filename, 'r') as csvfile:
         number_of_rows = number_of_rows+1
 
 if diff_exist:
-    Mail.send(MAIL_RECEIPIENTS, "noreply@nykaa.com", "GetPAS v2 and v3 comparison", "GetPAS v2 and v3 comparison contains diff.\nNo. of rows %s" % number_of_rows, diff_csv_filename, diff_csv_filename)
+    Mail.send(MAIL_RECEIPIENTS, "noreply@nykaa.com", "GetPAS v2 vs v3 Mismatch", "GetPAS v2 and v3 comparison contains diff.\nNo. of rows %s" % number_of_rows, diff_csv_filename, diff_csv_filename)
 else:
-    Mail.send(MAIL_RECEIPIENTS, "noreply@nykaa.com", "GetPAS v2 and v3 comparison", "GetPAS v2 and v3 comparison contains no diff.")
+    Mail.send(MAIL_RECEIPIENTS, "noreply@nykaa.com", "GetPAS v2 vs v3 Mismatch", "GetPAS v2 and v3 comparison contains no diff.")
 
 
 
