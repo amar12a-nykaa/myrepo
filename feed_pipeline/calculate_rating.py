28597e04fbec67d5747e7d5fefa90c81598109d3
Shweta2017 <shweta.agrawal@nykaa.com>
2020-07-28 09:49:12 +0530

    calculation change.
    Signed-off-by: Shweta2017 <shweta.agrawal@nykaa.com>

--- feed_pipeline/calculate_rating.py
+++ feed_pipeline/calculate_rating.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import arrow
 import datetime
+import json
 
 sys.path.append("/nykaa/scripts/sharedutils")
 from loopcounter import LoopCounter
@@ -40,19 +41,26 @@ def fetch_products_from_es(size):
 
 
 def get_avg_review(reviews):
+  reviews = json.loads(reviews)
   total_count = reviews.get("total_count", 0)
   if not total_count:
     return 0
   total_count += 5
   param1 = 0
   param2 = 0
+  param3 = 0
   z = 1.65
-  for i in range(1,5):
-    vote_i = reviews.get(str(i), {}).get('count', 0)
-    ratio = (vote_i+1)/total_count
-    param1 += i*ratio
-    param2 += i*param1
-  rating_value = param1 - z * pow((param2-pow(param1,2))/(total_count+1), 0.5)
+  for i in range(1,6):
+    vote_i = int(reviews.get(str(i-1), {}).get('count', 0))
+    ratio = i*((vote_i+1)/total_count)
+    param1 += ratio
+    param2 += i*ratio
+    param3 += pow(ratio, 2)
+
+  #print(param1, param2, param3, total_count)
+  #rating_value = param1 - z * pow((param2-pow(param1,2))/(total_count+1), 0.5)
+  rating_value = param1 - z * pow((param2-param3)/(total_count+1), 0.5)
+  #print(rating_value)
   return rating_value
 
 
@@ -70,6 +78,8 @@ def calculate_rating():
       try:
         product_id = docs['_source']['product_id']
         reviews = docs['_source'].get('review_splitup', {})
+        if not reviews:
+          continue
         rating_value = get_avg_review(reviews)
         filt = {"product_id": product_id}
         ret = rating_data.update_one(filt, {"$set": {'rating_value': rating_value, 'last_calculated': timestamp}}, upsert=True)
